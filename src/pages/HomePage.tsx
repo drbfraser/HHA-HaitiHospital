359c905512b33111278bb6c3c4163030b9698624
Long K. Tran <khanh_long_tran@sfu.ca>
2021-10-04 11:38:15 -0700

    INTERGRATE NEIL AND LONG HOMEPAGE
    merge Neil Home component into Long HomePage component

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,10 +1,22 @@
+import * as React from "react";
+import { Link, RouteComponentProps } from "react-router-dom";
+
+require("./HomeStyle.css");
+
+import case1 from "./img/case1.jpg"
+import case2 from "./img/case2.jpg"
+
 import Header from '../components/Header'
 import LeaderBar from '../components/LeaderBar'
 import MessageBoard from '../components/MessageBoard';
 import { MyCustomCSS } from '../components/MyCustomCSS';
 
-const HomePage = () => {
-  return (
+interface Props extends RouteComponentProps {}
+
+const HomePage: React.FC<Props> = ({ history, location, match }) => {
+    console.log(match, location);
+    
+    return (
     <>
       <Header classes='header grid'
         style={{'grid-template-columns': '2fr 1fr 1fr'} as MyCustomCSS}
@@ -13,6 +25,36 @@ const HomePage = () => {
         style={{'grid-template-columns': '2fr 1fr 1fr'} as MyCustomCSS}
       />
       <MessageBoard classes='message-board'/>
+
+      <div className="depart">
+            <div>This is the second half of the home page</div>
+            <p>-------------------------------- my part starts here --------------------------------</p>
+            {/*<Link to="/department">toDepart</Link>*/}
+            <button className="button1"
+                onClick={() => {
+                    // api call
+                    // change to the about page
+                    history.push("/departmentOne");
+                }}>NICU / PAED</button>
+            <button className="button2"
+                    onClick={() => {
+                        history.push("/departmentTwo");
+                    }}>MATERNITY</button>
+            <button className="button3">REHAB</button>
+            <button className="button4">COM-HEALTH</button>
+            <button className="button5">MORE</button>
+
+            <div>
+                <button className="caseStudyButton"
+                onClick={() => {
+                    history.push("./caseStudyMain");
+                }}>Case Study</button>
+            </div>
+            <div className="caseStudy-image">
+                <img src={case1} className="caseOne" alt="case1"/>
+                <img src={case2} className="caseTwo" alt="case2" />
+            </div>
+        </div>
     </>
   );
 }
