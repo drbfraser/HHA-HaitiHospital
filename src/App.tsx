4e70c03867c0d0960833305c97fcbace7a347547
Long K. Tran <khanh_long_tran@sfu.ca>
2021-10-04 11:44:35 -0700

    INTERGRATE NEIL AND LONG HOMEPAGE
    merge Neil App component into Long App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
 import React from 'react';
-import './App.css';
 import { BrowserRouter as Router, Route} from 'react-router-dom';
+
+import './App.css';
+
 import HomePage from './pages/HomePage'
+import { DepartmentOne } from "./pages/DepartmentOne";
+import { DepartmentTwo } from "./pages/DepartmentTwo";
+import { CaseStudyMain} from "./pages/CaseStudyMain";
+import { Post } from "./Post";
 
 function App() {
   return (
     <Router>
       <div className="app">
         <Route exact path='/home' component={HomePage}/>
+        <Route path="/departmentOne" exact component={DepartmentOne} />
+        <Route path="/departmentTwo" exact component={DepartmentTwo} />
+        <Route path='/caseStudyMain' exact component={CaseStudyMain} />
+        {/*<Route path="/posts/:id" exact component={Post} />*/}
+        {/*<Route path="/" render={() => <div>404</div>} />*/}
       </div>
     </Router>
   );
