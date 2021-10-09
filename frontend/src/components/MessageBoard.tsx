7c97be2cfdb0a6def4cf2a2a8bf9f1de1a400c51
Neil <sjc27@sfu.ca>
2021-10-09 11:49:58 -0700

    adjust the layout of message board section in home page

--- frontend/src/components/MessageBoard.tsx
+++ frontend/src/components/MessageBoard.tsx
@@ -1,24 +1,21 @@
 import {IProps} from './IProps';
 import TextHolder from './TextHolder';
-// import Btn from './Btn';
+import Btn from './Btn';
 import { Link, NavLink, RouteComponentProps } from "react-router-dom";
 
-interface IMessageBoard extends IProps {
-}
+interface IMessageBoard extends IProps {}
 
-const MessageBoard: React.FC<IProps> = (props: IMessageBoard) => {
-
+const MessageBoard: React.FC<IProps> = (props: IMessageBoard,) => {
   return (
     <div className={props.classes}>
-      <TextHolder classes='msgboard-title' text='MESSAGE BOARD'/>
-        {/*<Btn classes='msg-seemore' value='See more'/>*/}
-        {/*<button className="Departmentbutton2"*/}
-        {/*        onClick={() => {*/}
-        {/*            history.push("/Department2Maternity");*/}
-        {/*        }}>MATERNITY</button>*/}
-      <TextHolder classes='msg' text='Message 1'/>   
-      <TextHolder classes='msg' text='Message 2'/>   
-      <TextHolder classes='msg' text='Message 3'/>
+          <TextHolder classes='msgboard-title' text='MESSAGE BOARD'/>
+            {/*<Btn classes='msg-seemore' value='See more'/>*/}
+          <TextHolder classes='msg' text='Message 1 Message 1 Message 1 Message 1 Message 1 Message 1 '/>
+          <TextHolder classes='msg' text='Message 2 Message 2 Message 2 Message 2 Message 2 Message 2'/>
+          <TextHolder classes='msg' text='Message 3 Message 3 Message 3 Message 3 Message 3 Message 3'/>
+        <NavLink className="msg-seemore" to="/MessageBoardMain" exact>
+            See More
+        </NavLink>
     </div> 
   )
 }
