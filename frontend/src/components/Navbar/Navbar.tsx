fa90884a36cd4bccd1ed76639b78b21a7fd86274
Neil <sjc27@sfu.ca>
2021-10-09 15:22:53 -0700

    adjust navbar

--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -8,22 +8,26 @@ const Navbar = () => {
         <nav className="navbar">
             {/* <NavLink to="/" className="home-button" ><a href="" className="logo"><img className="logo" src={logo} alt="" /></a></NavLink> */}
             
-            <NavLink className="home-button" to="/home">
+            <NavLink className="navbar-home-button" to="/home">
                 <img src={logo}
                 alt="logo"  height="100px" width="320px"/> 
             </NavLink>
-            <NavLink className="toLeadersBoard" to="/leadersBaord" exact>
-                LeadersBoard
-            </NavLink>
-            <NavLink className="toMessageBoard" to="/messgeBoard" exact>
-                MessageBoard
-            </NavLink>
-            <NavLink className="toDepartments" to="/DepartmentMain" exact>
-                Departments
-            </NavLink>
-            <NavLink className="toCaseStudy" to="/caseStudyMain" exact>
-                CaseStudy
-            </NavLink>
+
+            <div className="navbar-other-button">
+                <NavLink className="toLeadersBoard" to="/leadersBaord" exact>
+                    LeadersBoard
+                </NavLink>
+                <NavLink className="toMessageBoard" to="/messgeBoard" exact>
+                    MessageBoard
+                </NavLink>
+                <NavLink className="toDepartments" to="/DepartmentMain" exact>
+                    Departments
+                </NavLink>
+                <NavLink className="toCaseStudy" to="/caseStudyMain" exact>
+                    CaseStudy
+                </NavLink>
+            </div>
+
         </nav>
     )
 }
