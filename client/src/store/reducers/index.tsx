22a6cdca95ee674f5f90e702e54bca5c987dbc4c
Long K. Tran <khanh_long_tran@sfu.ca>
2021-10-09 17:07:11 -0700

    Convert JS to TS

---
+++ client/src/store/reducers/index.tsx
@@ -0,0 +1,16 @@
+// Commented out during JS to TS for future reference
+// import { combineReducers } from 'redux';
+
+// import authReducer from './authReducer';
+// import registerReducer from './registerReducer';
+// import userReducer from './userReducer';
+// import usersReducer from './usersReducer';
+// import messageReducer from './messageReducer';
+
+// export default combineReducers({
+//   auth: authReducer,
+//   register: registerReducer,
+//   message: messageReducer,
+//   user: userReducer,
+//   users: usersReducer,
+// });
