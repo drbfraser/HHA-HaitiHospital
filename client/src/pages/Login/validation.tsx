6471f95badfb9bdc4aa65de891fb2dda5c4a8064
Long K. Tran <khanh_long_tran@sfu.ca>
2021-10-09 17:48:53 -0700

    Frontend: Convert JS to TS

---
+++ client/src/pages/Login/validation.tsx
@@ -0,0 +1,12 @@
+// Comment out during JS to TS for future reference
+// import * as Yup from 'yup';
+
+// export const loginSchema = Yup.object({
+//   email: Yup.string()
+//     .email('Invalid email address')
+//     .required('Required'),
+//   password: Yup.string()
+//     .min(6, 'Must be 6 characters at minimum')
+//     .max(20, 'Must be 20 characters or less')
+//     .required('Required'),
+// });
