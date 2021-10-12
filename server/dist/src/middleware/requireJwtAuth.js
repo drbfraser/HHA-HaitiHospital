0341d952f226b0b82d53b52ab821064871724fde
Felix Lin <fla65@sfu.ca>
2021-10-12 12:02:55 -0700

    removed dist files

--- server/dist/src/middleware/requireJwtAuth.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-var passport_1 = __importDefault(require("passport"));
-var requireJwtAuth = passport_1.default.authenticate('jwt', { session: false });
-exports.default = requireJwtAuth;
