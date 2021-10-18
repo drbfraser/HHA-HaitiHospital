1d068cf70e2069983bf8f0efeae2c993e721e232
Long K. Tran <khanh_long_tran@sfu.ca>
2021-10-17 20:27:45 -0700

    rename to lowercase file names

---
+++ client/src/components/loader/loader.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import { ElementStyleProps } from 'constants/interfaces';
+
+import './styles.css';
+
+interface LoaderProps extends ElementStyleProps {
+
+};
+
+const Loader = (props : LoaderProps) => {
+  return (
+    <div className={"loader-container loader "+ (props.classes || '')}>
+      <h3 className="loader-content">Loading..</h3>
+    </div>
+  );
+};
+
+export default Loader;
---
+++ client/src/layout/layout.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+
+import { ElementStyleProps } from 'constants/interfaces';
+
+import 'components/layout/styles.css';
+
+interface LayoutProps extends ElementStyleProps {
+  children: any,
+};
+
+const Layout = (props: LayoutProps) => {
+  return (
+    <div className={'layout '+props.classes}></div>
+  );
+}
+
+export default Layout;
+
+// Comment out during JS to TS, for future reference
+// import React from 'react';
+// import PropTypes from 'prop-types';
+
+// import Navbar from '../components/Navbar/Navbar';
+// import Footer from '../components/Footer/Footer';
+// import './styles.css';
+
+// const Layout = ({ children }) => {
+//   return (
+//     <>
+//       <Navbar />
+//       <div className="container">{children}</div>
+//       <Footer />
+//     </>
+//   );
+// };
+
+// Layout.propTypes = {
+//   children: PropTypes.node.isRequired,
+// };
+
+// export default Layout;
