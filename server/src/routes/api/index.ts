3f110a7326cbe8a4f3534eefd630dd8a65849588
Andrew Chen <zca24@sfu.ca>
2021-10-18 12:38:50 -0700

    renamed

--- server/src/routes/api/index.ts
+++ server/src/routes/api/index.ts
@@ -1,11 +1,11 @@
 import { Router } from 'express';
 import usersRoutes from './users';
 import messagesRoutes from './messages';
-import NICUPaedsRouter from './nicuPaeds';
+import nicuPaedsRoutes from './nicuPaeds';
 const router = Router();
 
 router.use('/users', usersRoutes);
 router.use('/messages', messagesRoutes);
-router.use('/NicuPaeds',NICUPaedsRouter);
+router.use('/nicupaeds', nicuPaedsRoutes);
 
 export default router;
