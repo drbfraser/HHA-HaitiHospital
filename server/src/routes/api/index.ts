3ce843169a1953ca7108e638f7bfc09e196fabab
Jeremy Lee <jwl45@sfu.ca>
2021-11-01 12:41:34 -0700

    Established base API framework for messageboards

--- server/src/routes/api/index.ts
+++ server/src/routes/api/index.ts
@@ -3,10 +3,15 @@ import usersRoutes from './users';
 import messagesRoutes from './messages';
 import nicuPaedsRoutes from './nicuPaeds';
 import reportRouter from './report';
+import messageBoard from './messageBoard'
 const router = Router();
 
 router.use('/users', usersRoutes);
 router.use('/messages', messagesRoutes);
 router.use('/nicupaeds', nicuPaedsRoutes);
 router.use('/report',reportRouter);
+
+//TODO: Might have to change messageBoard routing based on dashboard and where it needs to go
+router.use('/messageBoard',messageBoard);
+
 export default router;
