367b1ed2a97dba10dc634cdcc48d71104f24086b
Jeremy Huang <cha147@sfu.ca>
2021-11-02 15:51:01 -0700

    change email validation to username

--- client/src/pages/login/validation.tsx
+++ client/src/pages/login/validation.tsx
@@ -4,8 +4,10 @@
 import * as Yup from 'yup';
 
 export const loginSchema = Yup.object({
-  email: Yup.string()
-    .email('Invalid email address')
+  username: Yup.string()
+    // .email('Invalid email address')
+    .min(2, 'Must be 2 characters at minimum')
+    .max(20, 'Must be 20 characters or less')
     .required('Required'),
   password: Yup.string()
     .min(6, 'Must be 6 characters at minimum')
