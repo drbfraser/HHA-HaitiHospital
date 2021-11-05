3ea18413a27c8f318731d3f6d69543950b0c384d
Jeremy Lee <jwl45@sfu.ca>
2021-11-05 10:48:19 -0700

    Fix #70 - Add First name and Last name to models + autoseed DepartmentDB

--- server/src/routes/api/messageboard.ts
+++ server/src/routes/api/messageboard.ts
@@ -8,11 +8,12 @@ router.get('/', async (req: any, res: any) => {
         .catch(err => res.status(400).json('Could not find any results: ' + err));
 });
 
-router.get('/:departmentId', async (req: any, res: any) => {
-    MessageBody.find({departmentId: req.params.departmentId}).sort({date : 'desc'})
-        .then(Reports => res.json(Reports))
-        .catch(err => res.status(400).json('Could not find any results: ' + err));
-});
+//REMOVED FOR NOW - MIGHT BRING DEPARTMENT SPECIFIC MESSAGES FUNCTION BACK LATER
+// router.get('/department/:departmentId', async (req: any, res: any) => {
+//     MessageBody.find({departmentId: req.params.departmentId}).sort({date : 'desc'})
+//         .then(Reports => res.json(Reports))
+//         .catch(err => res.status(400).json('Could not find any results: ' + err));
+// });
 
 router.get('/message/:messageId', async (req: any, res: any) => {
     MessageBody.findById(req.params.messageId)
@@ -25,6 +26,8 @@ router.route('/').post((req: any, res: any) => {
     const departmentId: Number = <Number>req.body.departmentId;
     const departmentName: String = req.body.departmentName;
     const authorId: Number = <Number>req.body.authorId;
+    const authorFirstName: String = req.body.authorFirstName;
+    const authorLastName: String = req.body.authorLastName;
     const date: Date = dateTime
     const messageBody: String = req.body.messageBody;
     //TODO: replace messageHeader with Document Type 
@@ -34,6 +37,8 @@ router.route('/').post((req: any, res: any) => {
         departmentId,
         departmentName,
         authorId,
+        authorFirstName,
+        authorLastName,
         date,
         messageBody,
         messageHeader
