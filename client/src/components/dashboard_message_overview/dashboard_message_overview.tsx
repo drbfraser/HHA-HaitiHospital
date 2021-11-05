5fa85f49dc6dbd9d3a9947574be6f2b6dfea915d
Neil Chen <sjc27@sfu.ca>
2021-11-05 14:44:04 -0700

    Improve format of displaying top 3 messages on the dashboard page based on the previous design

--- client/src/components/dashboard_message_overview/dashboard_message_overview.tsx
+++ client/src/components/dashboard_message_overview/dashboard_message_overview.tsx
@@ -4,6 +4,8 @@ import { useState } from 'react';
 
 import { MessageProps } from 'constants/interfaces';
 import Axios from 'axios';
+import { NavLink} from "react-router-dom";
+import './dashboard_message_overview.css'
 
 interface DashboardMessageProps extends ElementStyleProps {
     messages :MessageProps[],
@@ -24,42 +26,51 @@ const DashboardMessageOverview = (props : DashboardMessageProps) => {
         })
     }, [])
 
-    return (
-        <table className={'dashboard-message-overview '+ (props.classes || '')}>
-            <thead>
-                <tr>
-                    <th>MessageTitle</th>
-                    <th>User</th>
-                    <th>Date</th>
-                    <th>Message</th>
-                </tr>
-            </thead>
-            <tbody>{
-                (message.map((message, index) => {
-                    // Displaying top 3 messages
-                    if (index <= 2) {
-                        return(
-                            <tr key={index}>
-                                <td>
-                                    {message.messageHeader}
-                                </td>
-                                <td>
-                                    {message.authorId}
-                                </td>
-                                <td>
-                                    {message.date}
-                                </td>
-                                <td>
-                                    {message.messageBody}
-                                </td>
-                            </tr>
-                        )
-                    }
-                }) 
-                )}
-            </tbody>
-        </table>
-    )
+    return(
+         <div className={'dashboard-message-overview '+ (props.classes||'')}>
+             <div className="my-3 p-3 bg-body rounded shadow-sm">
+                 <h5 className="pb-2 mb-3">Message Board</h5>
+
+                 <div className="d-flex border-bottom pb-2 mb-0 row">
+                     <h6 className="text-secondary col-md-2">Title</h6>
+                     <h6 className="text-secondary col-md-2">User</h6>
+                     <h6 className="text-secondary col-md-2">Date</h6>
+                     <h6 className="text-secondary col">Message</h6>
+                 </div>
+
+                 <tbody className="d-flex text-muted">
+                     <div className="pb-3 mb-0 lh-sm w-100">{
+                         (message.map((message, index) => {
+                                 // Displaying top 3 messages
+                                 if (index <= 2) {
+                                     return(
+                                         <tr key={index} className="d-flex mt-3 row border-bottom">
+                                             <td className="text-dark col-md-2">
+                                                 {message.messageHeader}
+                                             </td>
+                                             <td className="text-dark col-md-2">
+                                                 {message.authorId}
+                                             </td>
+                                             <td className="text-dark col-md-2">
+                                                 {message.date}
+                                             </td>
+                                             <p className="flex-column text-dark small col break-text">
+                                                 {message.messageBody.slice(0, 33)}...
+                                             </p>
+                                         </tr>
+                                     )
+                                 }
+                             })
+                         )}
+                     </div>
+                 </tbody>
+
+                 <small className="d-block text-end mt-3">
+                    <NavLink to="/messageBoard">See More</NavLink>
+                 </small>
+             </div>
+         </div>
+     )
 }
 
 export default DashboardMessageOverview;
