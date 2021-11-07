f00e70a33226833802c6c40e040fe512df34faf4
Long K. Tran <khanh_long_tran@sfu.ca>
2021-11-07 11:10:21 -0800

    Implement UI to message main page

--- client/src/components/message_panel/message_panel.tsx
+++ client/src/components/message_panel/message_panel.tsx
@@ -2,12 +2,15 @@ import React, { useEffect, useState } from 'react'
 
 import {ElementStyleProps, Json} from 'constants/interfaces'
 import Axios from 'axios'
+import MessageDisplay  from './message_display';
 
 interface MessagePanelProps extends ElementStyleProps {
 
 }
 
 const MessagePanel = (props: MessagePanelProps) => {
+
+    const [count, setCount] = useState<number>(5);
 
     const [msgsJson, setMsgJson] = useState<Json[]>([]);
     const dbApiForMessageBoard = '/api/messageBoard/'
@@ -26,7 +29,7 @@ const MessagePanel = (props: MessagePanelProps) => {
             apiSource.cancel();
             isMounted = false;
         }
-    })
+    }, [count])
 
     const fetchMsgs = async() => {
         try {
@@ -44,37 +47,32 @@ const MessagePanel = (props: MessagePanelProps) => {
 
     
     return (<>
-        {/* <div className="my-3 p-3 bg-body rounded shadow-sm">
+        <div className="my-3 p-3 bg-body rounded shadow-sm">
             <h6 className="border-bottom pb-2 mb-0">Recent updates</h6>
-            <div className="d-flex text-muted pt-3">
-            <svg className="bd-placeholder-img flex-shrink-0 me-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 32x32" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#007bff"></rect><text x="50%" y="50%" fill="#007bff" dy=".3em">32x32</text></svg>
-
-            <p className="pb-3 mb-0 small lh-sm border-bottom">
-                <strong className="d-block text-gray-dark">@username</strong>
-                Some representative placeholder content, with some information about this user. Imagine this being some sort of status update, perhaps?
-            </p>
-            </div>
-            <div className="d-flex text-muted pt-3">
-            <svg className="bd-placeholder-img flex-shrink-0 me-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 32x32" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#e83e8c"></rect><text x="50%" y="50%" fill="#e83e8c" dy=".3em">32x32</text></svg>
-
-            <p className="pb-3 mb-0 small lh-sm border-bottom">
-                <strong className="d-block text-gray-dark">@username</strong>
-                Some more representative placeholder content, related to this other user. Another status update, perhaps.
-            </p>
-            </div>
-            <div className="d-flex text-muted pt-3">
-            <svg className="bd-placeholder-img flex-shrink-0 me-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 32x32" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#6f42c1"></rect><text x="50%" y="50%" fill="#6f42c1" dy=".3em">32x32</text></svg>
-
-            <p className="pb-3 mb-0 small lh-sm border-bottom">
-                <strong className="d-block text-gray-dark">@username</strong>
-                This user also gets some representative placeholder content. Maybe they did something interesting, and you really want to highlight this in the recent updates.
-            </p>
-            </div>
-            <small className="d-block text-end mt-3">
+            {msgsJson.map((msgJson, index) => 
+                {
+                    if (index < count)
+                        return (<MessageDisplay key={index} msgJson={msgJson}/>)
+                }
+            )}
+           
+           
+            {/* <small className="d-block text-end mt-3">
             <a href="#">All updates</a>
-            </small>
-        </div> */}
+            </small> */}
+            <div className='d-md-flex flex-grow-1 justify-content-end'>
+                <button
+                onClick= {()=>(count <= msgsJson.length)&& setCount(count + 5)}>
+                    More
+                </button>
+                <button
+                onClick = {()=> (count > 0)&& setCount(count-5)}
+                >Less</button>
+            </div>
+        </div>
     </>);
 }
 
-export default MessagePanel
+export default MessagePanel;
+
+
