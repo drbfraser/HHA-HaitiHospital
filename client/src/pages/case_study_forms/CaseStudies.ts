8454157dfded437f686ae0e9a89265dab042bb91
Jeremy Huang <cha147@sfu.ca>
2021-11-08 00:06:09 -0800

    fix ts warnings

--- client/src/pages/case_study_forms/CaseStudies.ts
+++ client/src/pages/case_study_forms/CaseStudies.ts
@@ -7,50 +7,50 @@ enum CaseStudyOptions {
 }
 
 interface PatientStory {
-        patientsName: { type: String, required: true },
-        patientsAge: { type: Number, required: true },
-        whereIsThePatientFrom: { type: String, required: true },
-        whyComeToHCBH: { type: String, required: true },
-        howLongWereTheyAtHCBHinDays: { type: Number, required: true },
-        diagnosis: { type: String, required: true },
-        caseStudyStory: { type: String, required: true },
+        patientsName: { type: string, required: true },
+        patientsAge: { type: number, required: true },
+        whereIsThePatientFrom: { type: string, required: true },
+        whyComeToHCBH: { type: string, required: true },
+        howLongWereTheyAtHCBHinDays: { type: number, required: true },
+        diagnosis: { type: string, required: true },
+        caseStudyStory: { type: string, required: true },
 }
 
 interface StaffRecognition {
-        staffName: { type: String, required: true },
-        jobTitle: { type: String, required: true },
-        department: { type: String, required: true },
-        howManyMonthsWorkingAtHCBH: { type: Number, required: true },
-        mostEnjoy: { type: String, required: true },
-        caseStudyStory: { type: String, required: true },
+        staffName: { type: string, required: true },
+        jobTitle: { type: string, required: true },
+        department: { type: string, required: true },
+        howManyMonthsWorkingAtHCBH: { type: number, required: true },
+        mostEnjoy: { type: string, required: true },
+        caseStudyStory: { type: string, required: true },
 }
 
 interface TrainingSession {
         trainingDate: { type: Date, required: true },
-        trainingOn: { type: String, required: true },
-        whoConducted: { type: String, required: true },
-        whoAttended: { type: String, required: true },
-        benefitsFromTraining: { type: String, required: true },
-        caseStudyStory: { type: String, required: true },
+        trainingOn: { type: string, required: true },
+        whoConducted: { type: string, required: true },
+        whoAttended: { type: string, required: true },
+        benefitsFromTraining: { type: string, required: true },
+        caseStudyStory: { type: string, required: true },
 }
 
 interface EquipmentReceived {
-        equipmentReceived: { type: String, required: true },
-        departmentIdReceived: { type: Number, required: true },
-        whoSentEquipment: { type: String, required: true },
-        purchasedOrDonated: { type: String, required: true },
-        whatDoesEquipmentDo: { type: String, required: true },
-        caseStudyStory: { type: String, required: true },
+        equipmentReceived: { type: string, required: true },
+        departmentIdReceived: { type: number, required: true },
+        whoSentEquipment: { type: string, required: true },
+        purchasedOrDonated: { type: string, required: true },
+        whatDoesEquipmentDo: { type: string, required: true },
+        caseStudyStory: { type: string, required: true },
 }
 
 interface OtherStory {
-        caseStudyStory: { type: String, required: true },
+        caseStudyStory: { type: string, required: true },
 }
 
 export interface CaseStudyModel{
         caseStudyType: { type: CaseStudyOptions, required: true },
         // TODO: add created by user. right now JWT is not yet applied
-        // createdByUser: { type: String},
+        // createdByUser: { type: string},
         patientStory: PatientStory,
         staffRecognition: StaffRecognition,
         trainingSession: TrainingSession,
