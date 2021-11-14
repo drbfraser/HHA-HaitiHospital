515998d858fee5c2e2ad0cedd117d71abbfe711a
Jeremy Huang <jeremychhuang@gmail.com>
2021-11-14 02:50:15 -0800

    add leaderboard api

--- server/src/routes/api/leaderboard.ts
+++ server/src/routes/api/leaderboard.ts
@@ -1,15 +1,15 @@
 import { Router, Request, Response, NextFunction } from 'express';
 
 import requireJwtAuth from '../../middleware/requireJwtAuth';
-import CaseStudy from '../../models/CaseStudies';
-import Leader, { updateLeaderboard } from '../../models/Leader';
+import Department from '../../models/Leaderboard';
+import { updateDepartmentPoints } from '../../utils/leaderboard-utils'
 
 const router = Router();
 
 router.get('/', async (req, res) => {
     try {
-        updateLeaderboard();
-        Leader.find()
+        await updateDepartmentPoints();
+        Department.find()
             .then(data => res.json(data))
             .catch(err => res.status(400).json('Failed to get leaderboard: ' + err));
     } catch (err) {
@@ -18,32 +18,3 @@ router.get('/', async (req, res) => {
 });
 
 export default router;
-
-        // var pointsNicuPaeds = 0;
-        // await CaseStudy.find({ userDepartment: 0 }).exec(function (err, results) {
-        //     var count = results.length;
-        //     pointsNicuPaeds += count;
-        // });
-        // var pointsMaternity = 0;
-        // await CaseStudy.find({ userDepartment: 1 }).exec(function (err, results) {
-        //     var count = results.length;
-        //     pointsMaternity += count;
-        // });
-        // var pointsRehab = 0;
-        // await CaseStudy.find({ userDepartment: 2 }).exec(function (err, results) {
-        //     var count = results.length;
-        //     pointsRehab += count;
-        // });
-        // var pointsCommunity = 0;
-        // await CaseStudy.find({ userDepartment: 3 }).exec(function (err, results) {
-        //     var count = results.length;
-        //     pointsCommunity += count;
-        // });
-        // var data = {
-        //     nicuPaeds: pointsNicuPaeds,
-        //     maternity: pointsMaternity,
-        //     rehab: pointsRehab,
-        //     community: pointsCommunity,
-        // }
-        // await res.json(data);
-        // // await CaseStudy.find().then(data => res.json(data));
