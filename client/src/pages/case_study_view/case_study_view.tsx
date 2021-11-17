0d4d8a80d2649a611d36d7e7a76e05560221cb49
Jeremy Huang <jeremychhuang@gmail.com>
2021-11-16 19:25:04 -0800

    display all case study types

--- client/src/pages/case_study_view/case_study_view.tsx
+++ client/src/pages/case_study_view/case_study_view.tsx
@@ -61,26 +61,62 @@ export const CaseStudyView = (props: CaseStudyViewProps) => {
           </div>
         : null}
 
-        {/* {(caseStudy !== null && caseStudy.caseStudyType === CaseStudyOptions.StaffRecognition) ? 
+        {(caseStudy !== null && caseStudy.caseStudyType === CaseStudyOptions.StaffRecognition) ? 
           <div>
-            <h2>Patient Story Case Study</h2>
+            <h2>Staff Recognition Case Study</h2>
             <img src={ `../${caseStudy.imgPath}` } />
-            <h3>Patient's Name</h3>
-            <text>{caseStudy.patientStory.patientsName}</text>
-            <h3>Patient Age</h3>
-            <text>{caseStudy.patientStory.patientsAge}</text>
-            <h3>Where is the patient from?</h3>
-            <text>{caseStudy.patientStory.whereIsThePatientFrom}</text>
-            <h3>Why did the patient choose to come to HCBH?</h3>
-            <text>{caseStudy.patientStory.whyComeToHcbh}</text>
-            <h3>How long were they at HCBH?</h3>
-            <text>{caseStudy.patientStory.howLongWereTheyAtHcbh}</text>
-            <h3>What was their diagnosis?</h3>
-            <text>{caseStudy.patientStory.diagnosis}</text>
+            <h3>Staff Name</h3>
+            <text>{caseStudy.staffRecognition.staffName}</text>
+            <h3>Role / Job Title</h3>
+            <text>{caseStudy.staffRecognition.jobTitle}</text>
+            <h3>Which department do they work in?</h3>
+            <text>{caseStudy.staffRecognition.department}</text>
+            <h3>How long have they been working at HCBH?</h3>
+            <text>{caseStudy.staffRecognition.howLongWorkingAtHcbh}</text>
+            <h3>What do they enjoy the most about working at HCBH?</h3>
+            <text>{caseStudy.staffRecognition.mostEnjoy}</text>
             <h3>Case Study/Story</h3>
-            <text>{caseStudy.patientStory.caseStudyStory}</text>
+            <text>{caseStudy.staffRecognition.caseStudyStory}</text>
           </div>
-        : null} */}
+        : null}
+
+        {(caseStudy !== null && caseStudy.caseStudyType === CaseStudyOptions.TrainingSession) ? 
+          <div>
+            <h2>Training Session Case Study</h2>
+            <img src={ `../${caseStudy.imgPath}` } />
+            <h3>Training Date</h3>
+            <text>{caseStudy.trainingSession.trainingDate}</text>
+            <h3>What was the training on?</h3>
+            <text>{caseStudy.trainingSession.trainingOn}</text>
+            <h3>Who conducted training?</h3>
+            <text>{caseStudy.trainingSession.whoConducted}</text>
+            <h3>Who attended the training?</h3>
+            <text>{caseStudy.trainingSession.whoAttended}</text>
+            <h3>How will the training benefit HCBH and its staff?</h3>
+            <text>{caseStudy.trainingSession.benefitsFromTraining}</text>
+            <h3>Case Study/Story</h3>
+            <text>{caseStudy.trainingSession.caseStudyStory}</text>
+          </div>
+        : null}
+
+        {(caseStudy !== null && caseStudy.caseStudyType === CaseStudyOptions.EquipmentReceived) ? 
+          <div>
+            <h2>Equipment Received Case Study</h2>
+            <img src={ `../${caseStudy.imgPath}` } />
+            <h3>What equipment was received?</h3>
+            <text>{caseStudy.equipmentReceived.equipmentReceived}</text>
+            <h3>Which department received it?</h3>
+            <text>{caseStudy.equipmentReceived.departmentReceived}</text>
+            <h3>Who was the equipment from?</h3>
+            <text>{caseStudy.equipmentReceived.whoSentEquipment}</text>
+            <h3>Was it donated or purchased?</h3>
+            <text>{caseStudy.equipmentReceived.purchasedOrDonated}</text>
+            <h3>What does this new equipment do?</h3>
+            <text>{caseStudy.equipmentReceived.whatDoesEquipmentDo}</text>
+            <h3>Case Study/Story</h3>
+            <text>{caseStudy.equipmentReceived.caseStudyStory}</text>
+          </div>
+        : null}
 
         {(caseStudy !== null && caseStudy.caseStudyType === CaseStudyOptions.OtherStory) ? 
         <div>
