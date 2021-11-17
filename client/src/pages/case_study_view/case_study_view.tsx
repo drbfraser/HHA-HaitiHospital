621b5fbfd614c9cdebc11382c85875f4a7ecd207
Jeremy Huang <jeremychhuang@gmail.com>
2021-11-16 19:11:02 -0800

    [anuvrats] add case study display patient story

--- client/src/pages/case_study_view/case_study_view.tsx
+++ client/src/pages/case_study_view/case_study_view.tsx
@@ -4,6 +4,7 @@ import { ElementStyleProps } from "constants/interfaces";
 import SideBar from 'components/side_bar/side_bar';
 import Header from 'components/header/header'
 import axios from 'axios';
+import { CaseStudyOptions } from '../case_study_forms/CaseStudies';
 
 interface CaseStudyViewProps extends ElementStyleProps {
 }
@@ -29,24 +30,65 @@ export const CaseStudyView = (props: CaseStudyViewProps) => {
   // console.log(caseStudy);
 
   return (
-    // <div className={'case-study-view '+ props.classes}>
-    // <SideBar/>
-    // <main className="container">
-    //   <Header/>
-    //   <div>
-    //     <h3>Case Study/Story</h3>
-    //     <text>{caseStudy.otherStory.caseStudyStory}</text>
-    //   </div>
-    // </main>
-    // </div>
     <div className={'case-study-main '+ props.classes}>
       <SideBar/>
       <main className="container">
         <Header/>
-        {caseStudy !== null ? 
+        <div className="col-lg-2 col-md-3 col-sm-4 col-4">
+          <button type="button" className="btn btn-primary btn-md" onClick={() => {
+            props.history.push("/caseStudyMain");
+          }}>Back</button>
+        </div>
+
+        {(caseStudy !== null && caseStudy.caseStudyType === CaseStudyOptions.PatientStory) ? 
+          <div>
+            <h2>Patient Story Case Study</h2>
+            <img src={ `../${caseStudy.imgPath}` } />
+            <h3>Patient's Name</h3>
+            <text>{caseStudy.patientStory.patientsName}</text>
+            <h3>Patient Age</h3>
+            <text>{caseStudy.patientStory.patientsAge}</text>
+            <h3>Where is the patient from?</h3>
+            <text>{caseStudy.patientStory.whereIsThePatientFrom}</text>
+            <h3>Why did the patient choose to come to HCBH?</h3>
+            <text>{caseStudy.patientStory.whyComeToHcbh}</text>
+            <h3>How long were they at HCBH?</h3>
+            <text>{caseStudy.patientStory.howLongWereTheyAtHcbh}</text>
+            <h3>What was their diagnosis?</h3>
+            <text>{caseStudy.patientStory.diagnosis}</text>
+            <h3>Case Study/Story</h3>
+            <text>{caseStudy.patientStory.caseStudyStory}</text>
+          </div>
+        : null}
+
+        {/* {(caseStudy !== null && caseStudy.caseStudyType === CaseStudyOptions.StaffRecognition) ? 
+          <div>
+            <h2>Patient Story Case Study</h2>
+            <img src={ `../${caseStudy.imgPath}` } />
+            <h3>Patient's Name</h3>
+            <text>{caseStudy.patientStory.patientsName}</text>
+            <h3>Patient Age</h3>
+            <text>{caseStudy.patientStory.patientsAge}</text>
+            <h3>Where is the patient from?</h3>
+            <text>{caseStudy.patientStory.whereIsThePatientFrom}</text>
+            <h3>Why did the patient choose to come to HCBH?</h3>
+            <text>{caseStudy.patientStory.whyComeToHcbh}</text>
+            <h3>How long were they at HCBH?</h3>
+            <text>{caseStudy.patientStory.howLongWereTheyAtHcbh}</text>
+            <h3>What was their diagnosis?</h3>
+            <text>{caseStudy.patientStory.diagnosis}</text>
+            <h3>Case Study/Story</h3>
+            <text>{caseStudy.patientStory.caseStudyStory}</text>
+          </div>
+        : null} */}
+
+        {(caseStudy !== null && caseStudy.caseStudyType === CaseStudyOptions.OtherStory) ? 
         <div>
-          <h3>Case Study/Story</h3>
-          <text>{caseStudy.otherStory.caseStudyStory}</text>
+          <div>
+            <h2>Other Story Case Study</h2>
+            <h3>Case Study/Story</h3>
+            <text>{caseStudy.otherStory.caseStudyStory}</text>
+          </div>
         </div>
         : null}
       </main>
