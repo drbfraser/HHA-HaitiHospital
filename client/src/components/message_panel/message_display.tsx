26448009988cf1f0cb87e7af21c7315a39a85ceb
Long K. Tran <khanh_long_tran@sfu.ca>
2021-11-19 14:02:48 -0800

    Placeholder for deleting message

--- client/src/components/message_panel/message_display.tsx
+++ client/src/components/message_panel/message_display.tsx
@@ -2,17 +2,44 @@ import React from 'react';
 import {Link} from 'react-router-dom'
 
 import { Json, ElementStyleProps } from 'constants/interfaces';
+import { deleteMessage } from 'actions/messageActions';
+import Axios from 'axios';
 
 interface MessageDisplayProps extends ElementStyleProps  {
     msgJson : Json;
+    notifyChange : Function;
 }
 
 const MessageDisplay = (props: MessageDisplayProps) => {
 
-    return (<div className="d-flex text-muted pt-2">
-        <svg className="bd-placeholder-img flex-shrink-0 me-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 32x32" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#007bff"></rect><text x="50%" y="50%" fill="#007bff" dy=".3em">32x32</text></svg>
+    async function deleteMessage(msgId: string) {
+        const success = await deleteMessageFromDb(msgId);
 
+        if (success === true)
+            props.notifyChange();
+    }
+
+    async function deleteMessageFromDb(msgId: string) : Promise<boolean> {
+        const deleteMsgApi = '';
+        try {
+            // const response = await Axios.get(deleteMsgApi);
+            return true; 
+        }
+        catch (err: any) {
+            console.log("Delete message failed");
+            return false;
+        }
+    }  
+
+    return (
+    <div className="d-flex text-muted pt-2">
+        {/* Profile pic */}
+        <svg className="bd-placeholder-img flex-shrink-0 me-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 32x32" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#007bff"></rect><text x="50%" y="50%" fill="#007bff" dy=".3em">32x32</text></svg>
+        
+        {/* Message content */}
         <div className="pb-3 mb-0 border-bottom flex-grow-1">
+
+            {/* Author Id, Deparment, Date */}
             <div className="d-md-flex justify-content-between text-gray-dark">
                 <p><strong className="text-gray-dark">@{props.msgJson.authorId}</strong></p>
                 <p><strong className="lh-sm">
@@ -23,21 +50,30 @@ const MessageDisplay = (props: MessageDisplayProps) => {
                 </strong></p>
             </div>
 
+            {/* Message title */}
             <div className='text-gray-dark'>
                 <p><strong>Title: {props.msgJson.messageHeader}</strong></p>
-
             </div>
 
+            {/* Message body with utility buttons */}
             <div className="d-md-flex justify-content-between text-gray-dark text-break">
                 <p className="lh-sm">
                     {props.msgJson.messageBody}
                 </p>
-                <p className='d-md-flex lh-sm'>
-                    <Link className='align-self-center' to="#"><button type='button' className='btn btn-md btn-outline-secondary'>
-                        <i className="bi bi-pencil"></i>
-                    </button></Link>
 
-                    <button type="button" className="btn btn-md btn-outline-secondary" >
+                <p className='d-md-flex lh-sm'>
+                    <Link className='align-self-center' to="#">
+                        <button 
+                        type='button' 
+                        className='btn btn-md btn-outline-secondary'>
+                            <i className="bi bi-pencil"></i>
+                        </button>
+                    </Link>
+
+                    <button 
+                    type="button" 
+                    className="btn btn-md btn-outline-secondary"
+                    onClick = {() => deleteMessage(props.msgJson["_id"] as string)} >
                         <i className='bi bi-trash'></i>
                     </button>
                 </p>
--- client/src/components/message_panel/message_panel.tsx
+++ client/src/components/message_panel/message_panel.tsx
@@ -12,6 +12,7 @@ interface MessagePanelProps extends ElementStyleProps {
 const MessagePanel = (props: MessagePanelProps) => {
 
     const [count, setCount] = useState<number>(5);
+    const [rerender, setRerender] = useState<boolean> (false);
 
     const [msgsJson, setMsgJson] = useState<Json[]>([]);
     const dbApiForMessageBoard = '/api/messageBoard/'
@@ -26,11 +27,12 @@ const MessagePanel = (props: MessagePanelProps) => {
         }
 
         getMsgs();
+
         return function leaveSite() {
             apiSource.cancel();
             isMounted = false;
         }
-    }, [count])
+    }, [count, rerender])
 
     const fetchMsgs = async() => {
         try {
@@ -46,9 +48,16 @@ const MessagePanel = (props: MessagePanelProps) => {
         }
     }
 
+    const toggleRerender = async() => {
+        setRerender(!rerender);
+        console.log("Rerendering....");
+    }
+
     
     return (<>
         <div className="my-3 p-3 bg-body rounded shadow-sm">
+
+            {/* Add message */}
             <div className="d-sm-flex align-items-center">
                 <h6 className="border-bottom pb-2 mb-0">Recent updates</h6>
                 <div className='ml-auto'>
@@ -60,15 +69,20 @@ const MessagePanel = (props: MessagePanelProps) => {
                         </button>
                     </Link>
                 </div>
-                
             </div>
+
+            {/* Messsage row */}
             {msgsJson.map((msgJson, index) => 
                 {
                     if (index < count)
-                        return (<MessageDisplay key={index} msgJson={msgJson}/>)
+                        return (<MessageDisplay 
+                            key={index} 
+                            msgJson={msgJson}
+                            notifyChange={toggleRerender}/>)
                 }
             )}
-           
+
+            {/* Expand/shrink buttons */}
             <div className='d-sm-flex jutify-content-end'>
                 <div className='ml-auto d-sm-flex'>
                     <button
