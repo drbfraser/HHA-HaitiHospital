c03b3d1d5c4bdb065a3329a35bae32bdcf6459c8
Andrew Chen <zca24@sfu.ca>
2021-11-23 22:09:12 -0800

    added date selector

--- client/src/pages/form/nicu_form.tsx
+++ client/src/pages/form/nicu_form.tsx
@@ -562,14 +562,44 @@ function DynamicForm() {
                 <Header />
 
                 <div className="d-flex justify-content-start">
-                        <button type="button" className="btn btn-primary btn-sm" onClick={() => {
-                            history.push("/Department1NICU");
-                        }}>Back</button>
+                    <button type="button" className="btn btn-primary btn-sm" onClick={() => {
+                        history.push("/Department1NICU");
+                    }}>Back</button>
                 </div>
 
                 <div className="py-3 text-start">
                     {/* <h2>NICU/Paediatrics Form</h2> */}
-                    <p className="lead">For: September 2021</p>
+                    <span className="lead">Date: </span>
+                    <select className="form-select form-select-sm" style={{ width: "auto", display: "inline-block" }}>
+                        <option selected>Month</option>
+                        <option value="1">January</option>
+                        <option value="2">Feburary</option>
+                        <option value="3">March</option>
+                        <option value="4">April</option>
+                        <option value="5">May</option>
+                        <option value="6">June</option>
+                        <option value="7">July</option>
+                        <option value="8">August</option>
+                        <option value="9">September</option>
+                        <option value="10">October</option>
+                        <option value="11">November</option>
+                        <option value="12">December</option>
+                    </select>
+                    <select className="form-select form-select-sm" style={{ width: "auto", display: "inline-block" }}>
+                        <option selected>Year</option>
+                        <option value="2021">2021</option>
+                        <option value="2020">2020</option>
+                        <option value="2019">2019</option>
+                        <option value="2018">2018</option>
+                        <option value="2017">2017</option>
+                        <option value="2016">2016</option>
+                        <option value="2015">2015</option>
+                        <option value="2014">2014</option>
+                        <option value="2013">2013</option>
+                        <option value="2012">2012</option>
+                        <option value="2011">2011</option>
+                        <option value="2010">2010</option>
+                    </select>
                 </div>
 
                 <div className="row g-3">
