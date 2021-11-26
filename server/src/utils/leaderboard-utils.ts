8a27f08163624153322890c53e6604e5e5ed011f
Jeremy Huang <jeremychhuang@gmail.com>
2021-11-26 15:10:58 -0800

    add try catch

--- server/src/utils/leaderboard-utils.ts
+++ server/src/utils/leaderboard-utils.ts
@@ -1,37 +1,26 @@
 import CaseStudy from '../models/CaseStudies';
-import Department from '../models/Leaderboard';
-import { DepartmentName } from '../models/Leaderboard'
+import Department, { DepartmentName } from '../models/Leaderboard';
 
 const pointsPerCaseStudy = 10;
 
 export async function updateDepartmentPoints() {
-    
-    for (let key in DepartmentName) {
-        let departmentName = DepartmentName[key];
-        var departmentPoints = 0;
-        var nCaseStudies = 0;
-        // await CaseStudy.find().populate('user').exec(async (err, items) => {
-        //     items.forEach(item => {
-        //         if (item.user.department === departmentName){
-        //             nCaseStudies += 1;
-        //             departmentPoints += pointsPerCaseStudy;
-        //         }
-        //     })
-        // })
-        const caseStudies = await CaseStudy.find().populate('user');
-        caseStudies.forEach(item => {
-            if (item.user.department === departmentName){
-                nCaseStudies += 1;
-                departmentPoints += pointsPerCaseStudy;
-            }
-        })
-        
-        // await CaseStudy.countDocuments({ userDepartment: departmentName }, function(err, count) {
-        //     nCaseStudies += count;
-        //     departmentPoints += count * pointsPerCaseStudy;
-        // })
-        Department.updateOne({ "name": departmentName }, { $set: { "points": departmentPoints, "nCaseStudies": nCaseStudies } })
-            .catch((err) => {console.log(err)});
-    }
+    try {
+        for (let key in DepartmentName) {
+            let departmentName = DepartmentName[key];
+            var departmentPoints = 0;
+            var nCaseStudies = 0;
 
+            const caseStudies = await CaseStudy.find().populate('user');
+            await caseStudies.forEach(item => {
+                if (item.user.department === departmentName){
+                    nCaseStudies += 1;
+                    departmentPoints += pointsPerCaseStudy;
+                }
+            })
+
+            await Department.updateOne({ "name": departmentName }, { $set: { "points": departmentPoints, "nCaseStudies": nCaseStudies } })
+        }
+    } catch (err) {
+        console.log(err);
+    }
 }
