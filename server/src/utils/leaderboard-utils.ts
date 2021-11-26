687231c3ce444d56c57bac61e55c4f5cec838df6
Jeremy Huang <jeremychhuang@gmail.com>
2021-11-26 15:07:15 -0800

    change leaderboard update function

--- server/src/utils/leaderboard-utils.ts
+++ server/src/utils/leaderboard-utils.ts
@@ -8,12 +8,28 @@ export async function updateDepartmentPoints() {
     
     for (let key in DepartmentName) {
         let departmentName = DepartmentName[key];
-        let departmentPoints = 0;
-        let nCaseStudies = 0;
-        await CaseStudy.countDocuments({ userDepartment: departmentName }, function(err, count) {
-            nCaseStudies += count;
-            departmentPoints += count * pointsPerCaseStudy;
+        var departmentPoints = 0;
+        var nCaseStudies = 0;
+        // await CaseStudy.find().populate('user').exec(async (err, items) => {
+        //     items.forEach(item => {
+        //         if (item.user.department === departmentName){
+        //             nCaseStudies += 1;
+        //             departmentPoints += pointsPerCaseStudy;
+        //         }
+        //     })
+        // })
+        const caseStudies = await CaseStudy.find().populate('user');
+        caseStudies.forEach(item => {
+            if (item.user.department === departmentName){
+                nCaseStudies += 1;
+                departmentPoints += pointsPerCaseStudy;
+            }
         })
+        
+        // await CaseStudy.countDocuments({ userDepartment: departmentName }, function(err, count) {
+        //     nCaseStudies += count;
+        //     departmentPoints += count * pointsPerCaseStudy;
+        // })
         Department.updateOne({ "name": departmentName }, { $set: { "points": departmentPoints, "nCaseStudies": nCaseStudies } })
             .catch((err) => {console.log(err)});
     }
