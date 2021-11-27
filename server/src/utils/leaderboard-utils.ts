733cb11860932f1492e4a489c0d6f360e6ee63e5
Jeremy Huang <jeremychhuang@gmail.com>
2021-11-27 12:53:09 -0800

    move casestudy find out of the loop

--- server/src/utils/leaderboard-utils.ts
+++ server/src/utils/leaderboard-utils.ts
@@ -5,12 +5,12 @@ const pointsPerCaseStudy = 10;
 
 export async function updateDepartmentPoints() {
     try {
+        const caseStudies = await CaseStudy.find().populate('user');
         for (let key in DepartmentName) {
             let departmentName = DepartmentName[key];
             var departmentPoints = 0;
             var nCaseStudies = 0;
 
-            const caseStudies = await CaseStudy.find().populate('user');
             await caseStudies.forEach(item => {
                 if (item.user.department === departmentName){
                     nCaseStudies += 1;
