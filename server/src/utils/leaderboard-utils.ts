2125c49cca277871075ba8bb8459f64b4a45c7cf
Jeremy Huang <jeremychhuang@gmail.com>
2021-11-27 13:18:28 -0800

    record change

--- server/src/utils/leaderboard-utils.ts
+++ server/src/utils/leaderboard-utils.ts
@@ -6,19 +6,32 @@ const pointsPerCaseStudy = 10;
 export async function updateDepartmentPoints() {
     try {
         const caseStudies = await CaseStudy.find().populate('user');
-        for (let key in DepartmentName) {
-            let departmentName = DepartmentName[key];
-            var departmentPoints = 0;
-            var nCaseStudies = 0;
-
-            await caseStudies.forEach(item => {
+        const departmentPointsMap = new Map();
+        const nCaseStudiesMap = new Map();
+        
+        await caseStudies.forEach(async (item) => {
+            for (let key in DepartmentName) {
+                let departmentName = DepartmentName[key];
                 if (item.user.department === departmentName){
-                    nCaseStudies += 1;
-                    departmentPoints += pointsPerCaseStudy;
+                    let departmentPoints = departmentPointsMap.get(departmentName);
+                    if (departmentPoints) {
+                        departmentPoints += pointsPerCaseStudy;
+                    } else {
+                        departmentPoints = pointsPerCaseStudy;
+                    }
+                    departmentPointsMap.set(departmentName, departmentPoints);
+                    let nCaseStudies = nCaseStudiesMap.get(departmentName);
+                    if (nCaseStudies){
+                        nCaseStudies += 1;
+                    } else {
+                        nCaseStudies = 1;
+                    }
+                    nCaseStudiesMap.set(departmentName, nCaseStudies);
                 }
-            })
-
-            await Department.updateOne({ "name": departmentName }, { $set: { "points": departmentPoints, "nCaseStudies": nCaseStudies } })
+            }
+        })
+        for (let key in DepartmentName){
+            await Department.updateOne({ "name": DepartmentName[key] }, { $set: { "points": departmentPointsMap.get(DepartmentName[key]), "nCaseStudies": nCaseStudiesMap.get(DepartmentName[key]) } })
         }
         
     } catch (err) {
