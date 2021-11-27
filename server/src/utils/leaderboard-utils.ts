604f6295714bf7aa5724c987ee15bee4a956b373
Jeremy Huang <jeremychhuang@gmail.com>
2021-11-27 13:59:36 -0800

    use promise.all for updating leaderboard

--- server/src/utils/leaderboard-utils.ts
+++ server/src/utils/leaderboard-utils.ts
@@ -5,22 +5,18 @@ const pointsPerCaseStudy = 10;
 
 export async function updateDepartmentPoints() {
     try {
-        const caseStudies = await CaseStudy.find().populate('user');
-        for (let key in DepartmentName) {
-            let departmentName = DepartmentName[key];
-            var departmentPoints = 0;
-            var nCaseStudies = 0;
-
-            await caseStudies.forEach(item => {
-                if (item.user.department === departmentName){
-                    nCaseStudies += 1;
-                    departmentPoints += pointsPerCaseStudy;
-                }
-            })
-
-            await Department.updateOne({ "name": departmentName }, { $set: { "points": departmentPoints, "nCaseStudies": nCaseStudies } })
-        }
-        
+        var caseStudies;
+        await Promise.all([
+            (async() => {
+                await Department.updateMany({}, { $set: { "points": 0, "nCaseStudies": 0 } });
+            })(),
+            (async() => {
+                caseStudies = await CaseStudy.find().populate('user');
+            })()
+        ]);
+        caseStudies.forEach(async (item) => {
+            Department.findOneAndUpdate({ "name": item.user.department }, { $inc : { "points" : pointsPerCaseStudy, "nCaseStudies" : 1 } }).exec();
+        })
     } catch (err) {
         console.log(err);
     }
