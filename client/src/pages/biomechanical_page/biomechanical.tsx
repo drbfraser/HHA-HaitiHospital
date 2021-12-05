f1b9ec71fc07b0e76806d001845ab2993fd20efc
anuvrats <anuvrat_sharma@sfu.ca>
2021-12-05 14:19:16 -0800

    added delete button

--- client/src/pages/biomechanical_page/biomechanical.tsx
+++ client/src/pages/biomechanical_page/biomechanical.tsx
@@ -1,12 +1,15 @@
 import { useEffect, useState } from "react";
 import { RouteComponentProps, Link } from "react-router-dom";
-import { ElementStyleProps } from "constants/interfaces";
+import { ElementStyleProps, Role } from "constants/interfaces";
 import SideBar from 'components/side_bar/side_bar';
 import Header from 'components/header/header'
 import axios from 'axios';
+import { renderBasedOnRole } from "actions/roleActions"
 
 import "./biomechanical.css";
 import {useTranslation} from "react-i18next";
+import { useAuthState } from "Context";
+
 
 interface BiomechanicalPageProps extends ElementStyleProps {
 }
@@ -16,10 +19,24 @@ interface BiomechanicalPageProps extends RouteComponentProps {}
 export const BiomechanicalPage = (props: BiomechanicalPageProps) => {
 
    const [BioReport, setBioReport] = useState([]);
+   const authState = useAuthState();
+
    const BioReportUrl = `/api/biomech/`;
    const getBioReport = async () => {
     const res = await axios.get(BioReportUrl);
     setBioReport(res.data);
+  }
+
+  const deleteCaseStudy = async (id) => {
+    try {
+      if (!window.confirm('Are you sure you want to delete this case study?')) {
+        throw new Error("Deletion cancelled")
+      }
+      const res = await axios.delete(BioReportUrl + '/' + id);
+      getBioReport();
+    } catch (err) {
+      console.log(err);
+    }
   }
 
 
@@ -67,6 +84,9 @@ export const BiomechanicalPage = (props: BiomechanicalPageProps) => {
                                         <td>{(new Date(item.createdAt)).toLocaleString('en-US', { timeZone: 'America/Los_Angeles' })}</td>
                                         <td>
                                         <Link to={'/bioMechView/' + item._id} className="link-primary text-decoration-none">View </Link>
+                                        {renderBasedOnRole(authState.userDetails.role, [Role.Admin, Role.MedicalDirector]) ? 
+                                        <a href="javascript:void(0)" className="link-primary text-decoration-none" onClick={() => deleteCaseStudy(item._id)}>Delete</a>
+                                        : null}
                                         </td>
                                     </tr>
                                     ))
