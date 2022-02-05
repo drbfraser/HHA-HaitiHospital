67e059ea541e2cf18f390484fdd693a30adef375
Matt Fung <official.matthiasfung@gmail.com>
2022-02-04 16:37:38 -0800

    Change months to use enum from str array

--- client/src/utils/dateFormatting.ts
+++ client/src/utils/dateFormatting.ts
@@ -1,5 +1,5 @@
 // Haiti is GMT-5 (EASTERN TIME ET)
-const MONTHS: string[] = [
+enum Month {
   'January',
   'February',
   'March',
@@ -12,22 +12,15 @@ const MONTHS: string[] = [
   'October',
   'November',
   'December',
-];
+}
+
 const currDate = new Date(new Date().toLocaleString('en-US', { timeZone: 'America/Cancun' }));
 const currMonth = currDate.getMonth();
 const currYear = currDate.getFullYear();
 const currMonthLastDay = new Date(currDate.getFullYear(), currDate.getMonth() + 1, 0).getDate();
 const currMonthLastDate = new Date(currYear, currMonth, currMonthLastDay, 23, 59, 59);
 const makeDateShort = (date: string): string => {
-  return MONTHS[parseInt(date.substring(5, 7)) - 1].concat(' ').concat(date.substring(0, 4));
+  return Month[parseInt(date.substring(5, 7)) - 1].concat(' ').concat(date.substring(0, 4));
 };
 
-export {
-  MONTHS,
-  currDate,
-  currMonth,
-  currYear,
-  currMonthLastDay,
-  currMonthLastDate,
-  makeDateShort,
-};
+export { Month, currDate, currMonth, currYear, currMonthLastDay, currMonthLastDate, makeDateShort };
