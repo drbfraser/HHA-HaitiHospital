c22d8c82291443613a6636f40e9e2659a41ee253
Alex Le <mql@sfu.ca>
2022-02-05 15:29:57 -0800

    Minor fixes

--- server/src/utils/seed.ts
+++ server/src/utils/seed.ts
@@ -23,7 +23,6 @@ export const seedDb = async () => {
 
 export const seedUsers = async () => {
   console.log('Seeding users...');
-
   try {
     [...Array(7).keys()].forEach(async (index, i) => {
       var foundUser = await User.findOne({ username: `user${index}` });
