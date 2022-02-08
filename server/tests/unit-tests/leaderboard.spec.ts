8ec3b9be7f2392967ca79212100d6f3453923ae0
Felix Lin <felix_lin@sfu.ca>
2022-02-08 03:36:20 +0000

    Resolve "Setup backend testing framework"

---
+++ server/tests/unit-tests/leaderboard.spec.ts
@@ -0,0 +1,33 @@
+import http from 'http';
+import { Application } from 'express';
+import { setupApp, setupHttpServer, attemptAuthentication, Accounts } from './testTools/mochaHooks';
+const expect = require('chai').expect;
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+chai.use(chaiHttp);
+
+let testApp: Application;
+let httpServer: http.Server;
+let agent: any;
+
+describe('getLeaderboard', () => {
+  before('Create a working server', (done) => {
+    testApp = setupApp();
+    httpServer = setupHttpServer(testApp);
+    agent = chai.request.agent(testApp);
+    attemptAuthentication(agent, done, Accounts.AdminUser);
+  });
+
+  after('Close a working server', () => {
+    httpServer.close();
+    agent.close();
+  });
+
+  it('should get all leaderboard points', (done) => {
+    agent.get('/api/leaderboard').end((err: any, res: any) => {
+      expect(err).to.be.null;
+      expect(res).to.have.status(200);
+      done();
+    });
+  });
+});
---
+++ server/tests/unit-tests/messageboard.spec.ts
@@ -0,0 +1,33 @@
+import http from 'http';
+import { Application } from 'express';
+import { setupApp, setupHttpServer, attemptAuthentication, Accounts } from './testTools/mochaHooks';
+const expect = require('chai').expect;
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+chai.use(chaiHttp);
+
+let testApp: Application;
+let httpServer: http.Server;
+let agent: any;
+
+describe('getMessageBoard', () => {
+  before('Create a working server', (done) => {
+    testApp = setupApp();
+    httpServer = setupHttpServer(testApp);
+    agent = chai.request.agent(testApp);
+    attemptAuthentication(agent, done, Accounts.AdminUser);
+  });
+
+  after('Close a working server', () => {
+    httpServer.close();
+    agent.close();
+  });
+
+  it('should get all messages from the messageboard successfully', (done) => {
+    agent.get('/api/message-board').end((err: any, res: any) => {
+      expect(err).to.be.null;
+      expect(res).to.have.status(200);
+      done();
+    });
+  });
+});
