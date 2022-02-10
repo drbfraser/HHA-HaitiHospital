712b07c5b1477e830e5f3d34465a1f2e2b04190e
Ethan <ehinchli@sfu.ca>
2022-02-09 23:50:06 -0800

    Send requests directly to server, don't proxy through React Dev server

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,16 +1,16 @@
 import ReactDOM from 'react-dom';
 import React, { Suspense } from 'react';
-// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-// import { Provider } from 'react-redux';
-// import { createStore, applyMiddleware, compose } from 'redux';
-// import thunk from 'redux-thunk';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
-
 import './i18n';
-
 import App from 'app';
+import axios from 'axios';
+
+if (process.env.NODE_ENV === "development") {
+  axios.defaults.baseURL = `http://${window.location.hostname}:5000`;
+  axios.defaults.withCredentials = true;
+}
 
 ReactDOM.render(
   <React.StrictMode>
@@ -20,39 +20,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root'),
 );
-
-// import App from './app';
-// import './index.css';
-// import rootReducer from './store/reducers';
-
-// Commented out during JS->TS since I'm uncertain
-// const initialState = {};
-
-// declare global {
-//   interface Window {
-//     __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
-//   }
-// }
-
-// const store = createStore(
-//   rootReducer,
-//   initialState,
-//   compose(
-//     applyMiddleware(thunk),
-//     (window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ &&
-//       window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__()) ||
-//       compose,
-//   ),
-// );
-
-// ReactDOM.render(
-//   // Commented out during JS->TS since I'm uncertain
-//   <Provider store={store}>
-//     <Router>
-//       <Switch>
-//         <Route path="/" component={App} />
-//       </Switch>
-//     </Router>
-//   </Provider>,
-//   document.getElementById('root'),
-// );
