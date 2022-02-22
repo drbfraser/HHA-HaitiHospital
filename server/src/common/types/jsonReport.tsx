4e770fe5b55b2cc6f31bc1d244a9a905a0794625
Long K. Tran <kltran@sfu.ca>
2022-02-22 15:30:48 -0800

    defineobjects of a report

--- server/src/common/types/jsonReport.tsx
+++ server/src/common/types/jsonReport.tsx
@@ -5,46 +5,37 @@ export interface JsonReportMeta {
     readonly submittedUserId: string;
 };
 
-export enum ItemType {
-    SA = "short answer",
-    N = "numeric",
-    YN = "yes no",
-    MCQ = "mcq",
-    PO = "pick one",
-    MCQ_OPTION = "mcq option",
-    SUM = "sum",
-    // SG = "survey generator"
+export interface JsonReportItemMeta {
+    readonly type: string;
 }
 
-export interface JsonReportItemMeta {
-    readonly type: ItemType;
-}
+export type JsonItemAnswer = Array<string>;
 
 interface JsonReportItem {
     readonly meta: JsonReportItemMeta;
     readonly description: string;
-    answer?: string;
+    readonly answer: Array<JsonItemAnswer>;
 }
 
 //Short Answer Item
-export interface JsonReportSaItem extends JsonReportItem {};
+// export interface JsonReportSaItem extends JsonReportItem {};
 //Numeric Item
 export interface JsonReportNItem extends JsonReportItem {};
 //Yes No item
-export interface JsonReportYnItem extends JsonReportItem {};
+// export interface JsonReportYnItem extends JsonReportItem {};
 //Mcq item
-export interface JsonReportMcqItem extends JsonReportItem {
-    readonly options: Array<JsonMcqOption>;
-    readonly hasUserInput: boolean;
-    other?: JsonMcqOption;
-};
+// export interface JsonReportMcqItem extends JsonReportItem {
+//     readonly options: Array<JsonMcqOption>;
+//     readonly hasUserInput: boolean;
+//     other?: JsonMcqOption;
+// };
 //Pick one item
-export interface JsonReportPoItem extends JsonReportMcqItem {}
+// export interface JsonReportPoItem extends JsonReportMcqItem {}
 
-export interface JsonMcqOption {
-    readonly description: string;
-    readonly value: string;
-}
+// export interface JsonMcqOption {
+//     readonly description: string;
+//     readonly value: string;
+// }
 //Sum item
 export interface JsonReportSumItem extends JsonReportItem {
     readonly numericItems: Array<JsonReportNItem>;
@@ -53,7 +44,6 @@ export interface JsonReportSumItem extends JsonReportItem {
 //Survey Generator item
 // export interface JsonReportSurveyItem extends JsonReportItem {
 //     readonly items: Array<JsonReportItem>
-//     readonly numSurveys: number;
 // }
 
 export interface JsonReportDescriptor {
