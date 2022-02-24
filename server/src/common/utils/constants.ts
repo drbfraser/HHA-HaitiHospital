e186dec3e8f28d9c6454f2aa0e9a6575984102b7
Long K. Tran <kltran@sfu.ca>
2022-02-24 08:21:17 -0800

    successfully generate a test json schema with typescript-json-schema library

--- server/src/common/utils/constants.ts
+++ server/src/common/utils/constants.ts
@@ -1,4 +1,4 @@
-export const PATH_TO_JSON_REPORT_TYPES = '../definitions/json_report.ts';
-export const PATH_TO_REPORT_TYPES = '../definitions/report.ts';
+export const PATH_TO_JSON_REPORT_TYPES = `${__dirname}/../definitions/json_report.ts`;
+export const PATH_TO_REPORT_TYPES = `${__dirname}/../definitions/report.ts`;
 
-export const JSON_REPORT_TYPE = 'JsonReprotDescriptor';
+export const JSON_REPORT_TYPE = 'JsonReportDescriptor';
