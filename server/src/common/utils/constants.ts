5a8b5507fcb58879baa1dd8f6712cbea452491c6
Long K. Tran <kltran@sfu.ca>
2022-02-24 10:08:02 -0800

    set a stringifier to stringify interface names

--- server/src/common/utils/constants.ts
+++ server/src/common/utils/constants.ts
@@ -1,4 +1,2 @@
 export const PATH_TO_JSON_REPORT_TYPES = `${__dirname}/../definitions/json_report.ts`;
 export const PATH_TO_REPORT_TYPES = `${__dirname}/../definitions/report.ts`;
-
-export const JSON_REPORT_TYPE = 'JsonReportDescriptor';
