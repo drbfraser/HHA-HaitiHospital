c8544ddbf91224b798a5a64b3cd1bed630245e44
Matt Fung <official.matthiasfung@gmail.com>
2022-03-11 22:20:43 -0800

    Implement viewing employee of the month

---
+++ client/src/components/employee_of_the_month_summary/employee_of_the_month_summary.tsx
@@ -0,0 +1,92 @@
+import { useEffect, useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import axios from 'axios';
+import { toast } from 'react-toastify';
+import { currMonth, currYear } from 'utils/dateFormatting';
+
+export const EmployeeOfTheMonthSummary = ({ employeeOfTheMonth }) => {
+  const { t: translateText } = useTranslation();
+  const [employeeOfTheMonthImage, setEmployeeOfTheMonthImage] = useState<string>('');
+
+  const getEmployeeOfTheMonthImage = async () => {
+    await axios
+      .get(`/api/image/${employeeOfTheMonth.imgPath.split('/')[2]}`, {
+        responseType: 'blob',
+      })
+      .then((response: any) => {
+        setEmployeeOfTheMonthImage(URL.createObjectURL(response.data));
+      })
+      .catch(() => {
+        toast.error('Unable to fetch image');
+      });
+  };
+
+  const translateMonth = (monthIndicator: number): string => {
+    switch (monthIndicator) {
+      case 0:
+        return translateText('monthJanuary');
+      case 1:
+        return translateText('monthFebruary');
+      case 2:
+        return translateText('monthMarch');
+      case 3:
+        return translateText('monthApril');
+      case 4:
+        return translateText('monthMay');
+      case 5:
+        return translateText('monthJune');
+      case 6:
+        return translateText('monthJuly');
+      case 7:
+        return translateText('monthAugust');
+      case 8:
+        return translateText('monthSeptember');
+      case 9:
+        return translateText('monthOctober');
+      case 10:
+        return translateText('monthNovember');
+      case 11:
+        return translateText('monthDecember');
+    }
+  };
+
+  useEffect(() => {
+    // Only execute once case study data has been successfully passed to this component
+    if (employeeOfTheMonth.imgPath !== undefined) getEmployeeOfTheMonthImage();
+  }, [employeeOfTheMonth]);
+
+  return (
+    <div className="mb-5">
+      <div style={{ display: 'flex', flex: '1 1 auto' }}>
+        <div className="w-100">
+          <h4 className="mt-3 mb-3 fw-bold">
+            {translateText('employeeOfTheMonthTitle').concat(
+              translateMonth(currMonth).concat(' ').concat(currYear.toString()),
+            )}
+          </h4>
+          <h6 className="fs-6 lh-base fw-bold">{translateText('employeeOfTheMonthDate')}</h6>
+          <p className="fs-6 lh-base text-break">
+            {new Date(employeeOfTheMonth.updatedAt).toLocaleDateString('en-US', {
+              timeZone: 'America/Cancun',
+            })}
+          </p>
+          <h6 className="fs-6 fw-bold lh-base">{translateText('employeeOfTheMonthName')}</h6>
+          <p className="fs-6 lh-base text-break">{employeeOfTheMonth.name}</p>
+          <h6 className="fs-6 fw-bold lh-base">{translateText('employeeOfTheMonthDepartment')}</h6>
+          <p className="fs-6 lh-base text-break">{employeeOfTheMonth.department}</p>
+          <h6 className="fs-6 fw-bold lh-base">{translateText('employeeOfTheMonthDescription')}</h6>
+          <p className="fs-6 lh-base text-break">{employeeOfTheMonth.description}</p>
+        </div>
+        <div>
+          <img
+            src={employeeOfTheMonthImage}
+            className={`img-thumbnail img-fluid mt-3 mb-3 ${
+              employeeOfTheMonth.imgPath ? 'd-block' : 'd-none'
+            }`}
+            alt="Employee Of The Month..."
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
--- client/src/pages/employee_of_the_month_main/employee_of_the_month_main.tsx
+++ client/src/pages/employee_of_the_month_main/employee_of_the_month_main.tsx
@@ -1,27 +1,57 @@
-import { useCallback, useEffect, useState } from 'react';
-import { RouteComponentProps, Link, useHistory } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { RouteComponentProps, Link } from 'react-router-dom';
 import { Role } from 'constants/interfaces';
 import SideBar from 'components/side_bar/side_bar';
 import Header from 'components/header/header';
-import ModalDelete from 'components/popup_modal/popup_modal_delete';
+import { EmployeeOfTheMonthSummary } from 'components/employee_of_the_month_summary/employee_of_the_month_summary';
 import axios from 'axios';
 import { toast } from 'react-toastify';
 import './employee_of_the_month.css';
 import { useTranslation } from 'react-i18next';
 import { useAuthState } from 'Context';
 import { renderBasedOnRole } from 'actions/roleActions';
-import i18n from 'i18next';
 
 interface EmployeeOfTheMonthMainProps extends RouteComponentProps {}
 
 export const EmployeeOfTheMonthMain = (props: EmployeeOfTheMonthMainProps) => {
+  const [employeeOfTheMonth, setEmployeeOfTheMonth] = useState([]);
+  const authState = useAuthState();
   const { t: translateText } = useTranslation();
+
+  const getEmployeeOfTheMonth = async () => {
+    await axios
+      .get('/api/employee-of-the-month')
+      .then((response: any) => {
+        console.log(response.data);
+        setEmployeeOfTheMonth(response.data);
+      })
+      .catch((err: any) => {
+        toast.error('Unable to fetch employee of the month');
+      });
+  };
+
+  useEffect(() => {
+    getEmployeeOfTheMonth();
+  }, []);
 
   return (
     <div className={'employee-of-the-month-main'}>
       <SideBar />
       <main className="container-fluid main-region">
         <Header />
+        {renderBasedOnRole(authState.userDetails.role, [Role.Admin, Role.MedicalDirector]) ? (
+          <div className="d-flex justify-content-start">
+            <Link to="/employee-of-the-month/form">
+              <button type="button" className="btn btn-outline-dark">
+                {translateText('employeeOfTheMonthEdit')}
+              </button>
+            </Link>
+          </div>
+        ) : null}
+
+        <div className="my-3 p-2 bg-body rounded shadow-sm mb-3">
+          <EmployeeOfTheMonthSummary employeeOfTheMonth={employeeOfTheMonth} />
+        </div>
       </main>
     </div>
   );
