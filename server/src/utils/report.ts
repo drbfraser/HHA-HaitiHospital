99d5938fe3c762976d10cc4573c61c2990f1d6f5
Long K. Tran <kltran@sfu.ca>
2022-03-26 21:28:30 -0700

    remove a file conflicts caused by rebasing

--- server/src/utils/report.ts
+++
@@ -1,9 +0,0 @@
-import { ReportDescriptor, ReportItems, ReportMeta } from "models/report"
-
-export const getReportMeta = (report: ReportDescriptor): ReportMeta => {
-    return report.meta;
-}
-
-export const getReportItems = (report: ReportDescriptor): ReportItems => {
-    return report.items;
-}
