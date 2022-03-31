43962daa243ab1c3d286d00016f067c798635c36
Matt Fung <official.matthiasfung@gmail.com>
2022-03-31 01:34:59 -0700

    Refactor mock department api for add report component to remove unnecessary use state

--- client/src/pages/add_report/add_report.tsx
+++ client/src/pages/add_report/add_report.tsx
@@ -10,26 +10,24 @@ import { Department } from 'constants/interfaces';
 import { History } from 'history';
 
 const AddReport = () => {
-  const [department, setDepartment] = useState<Department>(initialDepartments.departments[0]);
   const [departments, setDepartments] = useState<Department[]>(initialDepartments.departments);
   const history: History = useHistory<History>();
   const { deptId } = useParams<{ deptId: string }>();
 
   useEffect(() => {
     // For Future Devs: Replace MockDepartmentApi with Api
-    setDepartment(MockDepartmentApi.getDepartmentById(deptId) as Department);
     setDepartments(MockDepartmentApi.getDepartments());
   }, []);
 
   try {
-    switch (department) {
-      case departments[0]:
+    switch (deptId) {
+      case departments[0].id:
         return <RehabForm></RehabForm>;
-      case departments[1]:
+      case departments[1].id:
         return <NICUForm></NICUForm>;
-      case departments[2]:
+      case departments[2].id:
         return <MaternityForm></MaternityForm>;
-      case departments[3]:
+      case departments[3].id:
         return <CommunityForm></CommunityForm>;
     }
   } catch (e) {
