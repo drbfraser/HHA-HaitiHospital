f65a85bb1bf2314542d26e28860814706e3abcf0
Long K. Tran <kltran@sfu.ca>
2022-04-06 22:15:56 -0700

    fix constant paths

--- server/src/utils/constants.ts
+++ server/src/utils/constants.ts
@@ -1,2 +1,1 @@
-export const PATH_TO_JSON_REPORT_TYPES = `${__dirname}/../common/definitions/json_report.ts`;
-export const PATH_TO_REPORT_DEFINITIONS = `${__dirname}/../common/definitions/report.ts`;
+export const PATH_TO_JSON_REPORT_TYPES = `${__dirname}/../common/json_report.ts`;
