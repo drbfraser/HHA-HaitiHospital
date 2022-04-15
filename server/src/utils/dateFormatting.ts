8282cbcfb5ed584cecaa5080a3008f8f36f640f0
Matt Fung <official.matthiasfung@gmail.com>
2022-04-15 13:29:05 -0700

    Get rid of unused date utils

--- server/src/utils/dateFormatting.ts
+++ server/src/utils/dateFormatting.ts
@@ -19,8 +19,4 @@ const currMonth = currDate.getMonth();
 const currYear = currDate.getFullYear();
 const currMonthLastDay = new Date(currDate.getFullYear(), currDate.getMonth() + 1, 0).getDate();
 
-const makeDateShort = (date: string): string => {
-  return Month[parseInt(date.substring(5, 7)) - 1].concat(' ').concat(date.substring(0, 4));
-};
-
-export { Month, currDate, currMonth, currYear, currMonthLastDay, makeDateShort };
+export { Month, currDate, currMonth, currYear, currMonthLastDay };
