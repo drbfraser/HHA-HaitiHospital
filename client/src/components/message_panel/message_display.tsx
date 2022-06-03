45687dfa8e882cc55747668c9f0ca644d0ebce4e
Jeremy Huang <cha147@sfu.ca>
2022-06-02 23:04:41 -0700

    move comments button to beneath the message text

--- client/src/components/message_panel/message_display.tsx
+++ client/src/components/message_panel/message_display.tsx
@@ -142,16 +142,16 @@ const MessageDisplay = (props: MessageDisplayProps) => {
 
                 <p className="department-info">{translateText('messageBoardPostedOn')}</p>
                 <p className="department-info">{readableDate}</p>
-                <Link className="align-self-center" to={`/message-board/comments/${message.id}`}>
-                  <button type="button" className="btn btn-link text-decoration-none admin-utils">
-                    {translateText('messageBoardComments') + '(' + (commentCount ? commentCount : 0) + ')'}
-                  </button>
-                </Link>
               </div>
             </div>
           </div>
           <div className="mr-auto p-2">
             <p className="lh-sm message-body">{message.messageBody}</p>
+            <Link className="align-self-center" to={`/message-board/comments/${message.id}`}>
+              <button type="button" className="btn btn-link text-decoration-none admin-utils">
+                {translateText('messageBoardComments') + '(' + commentCount + ')'}
+              </button>
+            </Link>
           </div>
         </div>
       </div>
