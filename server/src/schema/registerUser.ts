fe68a1df968f6f923d03dc54d9c7cab2bb581218
Sahab <szafar@biointeractivetech.com>
2022-06-29 13:33:24 -0700

    fixed sanitization schema for department

--- server/src/schema/registerUser.ts
+++ server/src/schema/registerUser.ts
@@ -4,14 +4,13 @@ const mongoose = require('mongoose');
 
 const checkDepartment = () => {
   return check('department')
-      .exists()
       .custom((department) => {
           if ((department)) {
               [
                   check('department.id')
                       .notEmpty()
                       .custom(id => mongoose.isValidObjectId(id))
-                      .withMessage("Id must be non empty and a valid mongoose Id")
+                      .withMessage("Department Id must be non empty and a valid mongoose Id")
                   ,
                   check('department.name')
                       .notEmpty()
@@ -34,7 +33,6 @@ const userEdit = [
   check('name').exists().trim().escape().isLength({ min: 2, max: 30 }).withMessage(msgStringMulti(2, 30)),
   check('role').exists().trim().escape().withMessage('User role is expected'),
   check('department').custom(checkDepartment),
-  //check('department').exists().escape().trim().withMessage('User department is expectd'),
   check('password').if(check('password').exists().notEmpty()).escape().isLength({ min: 6, max: 20 }).withMessage(msgStringMulti(6, 20))
 ];
 
