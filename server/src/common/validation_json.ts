608896d155ca80f7afd753988c553dbcd53d4756
Daniel Maia <dgomesma@sfu.ca>
2022-07-09 23:07:46 -0700

    Add comments

--- server/src/common/validation_json.ts
+++ server/src/common/validation_json.ts
@@ -4,7 +4,15 @@ export interface JsonValidationError {
     errorMessage: string;
 }
 
+/*
+    Parameters:
+        id -  This is necessary for situations where identifying the data sent 
+        from the front is not obvious (e.g. when we have multiple fields of the 
+        same type or possibly nested items).
+
+        errors - Should be empty when no errors were found.
+*/
 export interface JsonValidationResults {
     id: string;
-    errors?: Array<JsonValidationError>
+    errors: Array<JsonValidationError>
 }
