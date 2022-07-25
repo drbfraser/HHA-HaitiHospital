9e43cb5c8bb65a39810575a58f41eb74909a9e45
Daniel Maia <dgomesma@sfu.ca>
2022-07-25 16:32:15 -0700

    Template unit tests for ObjectSerializer

--- client/tests/common/Serializer/ObjectSerializer.spec.ts
+++ client/tests/common/Serializer/ObjectSerializer.spec.ts
@@ -1,10 +1,38 @@
-var expect = require('chai').expect;
+import {ObjectSerializer, serializable} from '../../../src/common/Serializer/ObjectSerializer';
+import * as chai from 'chai';
+import * as sinon from 'sinon';
 
-describe('ObjectSerializer', () => {
-    describe('Test', () => {
-        it('Should be all good', () => {
-            console.log("Hello World!");
-            expect(true).to.be.true;
+
+describe('Serializer', function () {
+    describe("ObjectSerializer", function () {
+        describe("Get singleton", function () {
+            it('Should instantiate object on first call', function () {
+                // TODO: Implement
+            });
+
+            it('Should not instantiate object on second call and so forth', function () {
+                // TODO: Implement
+            });
+        });
+
+        describe('Serialization and Deserialization', function () {
+            it('Serialized serializable objects should be deserialized', function () {
+                // TODO: Implement
+            });
+
+            it('Non-serializable serialized objects should fail upon deserialization', function () {
+                // TODO: Implement
+            });
+
+            it('Serialized non-serializable objects should fail upon deserialization', function () {
+                // TODO: Implement
+            })
         });
     });
-});
+
+    describe(`Serializable decorator `, function () {
+        it('Should pass the class constructor to ObjectSerializer.addSerializable', function () {
+            // TODO: Implement
+        });
+    });
+})
