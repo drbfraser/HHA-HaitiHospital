9a7bd8131e0afb99b0544ac58ce02a1ffe4f1e19
Daniel Maia <dgomesma@sfu.ca>
2022-07-25 11:38:20 -0700

    Add chai and mocha unit testing

---
+++ client/tests/common/Serializer/ObjectSerializer.spec.ts
@@ -0,0 +1,10 @@
+var expect = require('chai').expect;
+
+describe('ObjectSerializer', () => {
+    describe('Test', () => {
+        it('Should be all good', () => {
+            console.log("Hello World!");
+            expect(true).to.be.true;
+        });
+    });
+});
