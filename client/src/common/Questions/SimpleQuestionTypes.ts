2b469be73bd6593d2c3f0417c0a529387acebb3f
Daniel Maia <dgomesma@sfu.ca>
2022-07-27 11:02:59 -0700

    Fix typo

--- client/src/common/Questions/SimpleQuestionTypes.ts
+++ client/src/common/Questions/SimpleQuestionTypes.ts
@@ -2,7 +2,7 @@ import { serializable } from "common/Serializer/ObjectSerializer";
 import { Question } from "./Question";
 
 @serializable(undefined, "")
-export class TestQuestion<ID> extends Question<ID, string> {}
+export class TextQuestion<ID> extends Question<ID, string> {}
 
 @serializable(undefined, "")
 export class NumericQuestion<ID> extends Question<ID, number> {}
