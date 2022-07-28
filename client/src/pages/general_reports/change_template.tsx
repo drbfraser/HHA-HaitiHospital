4cda63d659c27f56ff3a72982d1e2562ebb2d637
Jeremy Huang <cha147@sfu.ca>
2022-07-28 16:41:49 -0700

    add file upload

--- client/src/pages/general_reports/change_template.tsx
+++ client/src/pages/general_reports/change_template.tsx
@@ -1,21 +1,42 @@
 import { useState } from 'react';
 import { RouteComponentProps, Link } from 'react-router-dom';
-import { useForm } from 'react-hook-form';
+// import { useForm } from 'react-hook-form';
 import SideBar from 'components/side_bar/side_bar';
 import Header from 'components/header/header';
 import Api from 'actions/Api';
 import './general_reports_styles.css';
 import { useTranslation } from 'react-i18next';
 import { toast } from 'react-toastify';
-import { imageCompressor } from 'utils/imageCompressor';
+import { ENDPOINT_TEMPLATE_PUT } from 'constants/endpoints';
+import { TOAST_TEMPLATE_PUT } from 'constants/toast_messages';
 
 interface ChangeTemplateProps extends RouteComponentProps {}
 
 export const ChangeTemplate = (props: ChangeTemplateProps) => {
   const { t } = useTranslation();
-  const [selectedFile, setSelectedFile] = useState(null);
-  const onImageUpload = (item: File) => {
-    setSelectedFile(item);
+  const [selectedFile, setSelectedFile] = useState<File>(null);
+  // const onImageUpload = (item: File) => {
+  //   setSelectedFile(item);
+  // };
+
+  const handleSubmit = async () => {
+    let formData : FormData = new FormData();
+    formData.append('file', selectedFile);
+
+    // alert("stuff")
+    await Api.Put(
+      ENDPOINT_TEMPLATE_PUT,
+      formData,
+      onSubmitActions,
+      TOAST_TEMPLATE_PUT,
+      props.history,
+    );
+  };
+
+  const onSubmitActions = () => {
+    toast.success('Template successfully updated!');
+    setSelectedFile(null);
+    props.history.push('/change-template');
   };
 
   return (
@@ -31,7 +52,7 @@ export const ChangeTemplate = (props: ChangeTemplateProps) => {
             </button>
           </Link>
         </div>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className={`form-group col-md-6`}>
             <label className="form-label">{t('Upload New Template')}</label>
             <input
@@ -40,7 +61,7 @@ export const ChangeTemplate = (props: ChangeTemplateProps) => {
               className="form-control"
               id="customFile"
               required
-              onChange={(e) => imageCompressor(e.target.files[0], onImageUpload)}
+              onChange={(e) => setSelectedFile(e.target.files[0])}
             />
             <div className="mt-3 mb-5">
               <button className="btn btn-primary" type="submit">
