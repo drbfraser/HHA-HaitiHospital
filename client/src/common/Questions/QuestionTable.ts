0b02a8f4ba28094796d733397a0a00499de82c5b
Daniel Maia <dgomesma@sfu.ca>
2022-07-28 21:30:22 -0700

    Add cells to table

--- client/src/common/Questions/QuestionTable.ts
+++ client/src/common/Questions/QuestionTable.ts
@@ -5,25 +5,49 @@ import { QuestionItem } from "./QuestionItem";
 
 type table<T> = Array<Array<T>>;
 
+// The cell must be gray if no question is defined. 
+class TableCell<ID, T, QuestionType extends Question<ID, T>> {
+    private readonly question?: QuestionType;
+    private gray: boolean;
+    
+    constructor(question?: QuestionType) {
+        this.question = question ?? undefined;
+        this.gray = !question;
+    }
+
+    public readonly makeGrey = (): TableCell<ID, T, QuestionType> => {
+        this.gray = true;
+        return this;
+    }
+
+    public readonly ungrey = (): TableCell<ID, T, QuestionType> => {
+        this.gray = !this.question; 
+        return this;
+    }
+
+    public readonly isGray = (): boolean => this.gray;
+
+    public readonly getQuestion = (): QuestionType | undefined => {
+        return this.question;
+    }
+}
+
 @serializable(undefined, [], [], (row, col) => undefined)
 export class QuestionTable<ID, T, QuestionType extends Question<ID, T>> extends QuestionCollection<ID> {
 
     private readonly rowHeaders: Array<string>;
     private readonly columnHeaders: Array<string>;
 
-    private readonly questionTable: table<QuestionType>;
+    private readonly questionTable: table<TableCell<ID, T, QuestionType>>;
 
     /*  The questionCreator is a callback that takes in the row and column index
-        for where the question will be placed and returns a created question
-        of the type used in the table. The developer is free to choose how to
-        create that question and how to use the arguments given in the question
-        creation.
+        for where the question will be placed and may return a question. 
     */
     constructor(
         id: ID,
         rowHeaders: Array<string>,
         columnHeaders: Array<string>,
-        questionCreator: (row: number, col: number) => QuestionType,
+        questionCreator: (row: number, col: number) => QuestionType | undefined,
     ) {
         super(id);
         this.rowHeaders = [...rowHeaders];
@@ -32,14 +56,16 @@ export class QuestionTable<ID, T, QuestionType extends Question<ID, T>> extends 
         this.questionTable = (new Array(rowHeaders.length))
             .map((x, row) => new Array(columnHeaders.length)
                 .map((x, col) => questionCreator(row, col))
+                .map(question => new TableCell(question))
             );
     }
 
-    // Returns undefined if given numbers are out of bound
+    // Returns undefined if given numbers are out of bound OR if no question has
+    // been defined in the given cell.
     public readonly getQuestionAt = (row: number, col: number): QuestionType | undefined => {
-        return (row > 0 && row < this.questionTable.length)
-            && (col > 0 && col < this.questionTable[0].length) ?
-            this.questionTable[row][col] : undefined;
+        return (row >= 0 && row < this.questionTable.length)
+            && (col >= 0 && col < this.questionTable[0].length) ?
+            this.questionTable[row][col].getQuestion() : undefined;
     }
 
     public readonly getRowHeaders = (): Array<string> => [...this.rowHeaders];
@@ -49,6 +75,7 @@ export class QuestionTable<ID, T, QuestionType extends Question<ID, T>> extends 
     public readonly searchById = (id: ID): QuestionItem<ID> | undefined => {
         return this.questionTable
             .reduce((questions1, questions2) => [...questions1, ...questions2])
+            .map(questionCell => questionCell.getQuestion())
             .filter((questionItem) => questionItem.getId() == id)[0];
     }
 }
