b099fb047d5f6f0a5bc795c13371a5e90fec9847
Daniel Maia <dgomesma@sfu.ca>
2022-07-28 22:02:03 -0700

    Add CompositionQuestion

---
+++ client/src/common/Questions/CompositionQuestion.ts
@@ -0,0 +1,47 @@
+import { QuestionCollection } from "./QuestionCollection";
+import { NumericQuestion } from "./SimpleQuestionTypes";
+
+export class CompositionQuestion<ID> extends QuestionCollection<ID> {
+
+    private answer?: number;
+    private readonly questions: Array<NumericQuestion<ID>>;
+
+    constructor(id: ID, defaultAnswer?: number, ...questions: Array<NumericQuestion<ID>>) {
+        super(id);
+        this.answer = defaultAnswer;
+        questions ? this.addAll(...questions) : undefined;
+    }
+
+    public readonly searchById = (id: ID): NumericQuestion<ID> | undefined => {
+        return this.questions
+            .find(question => question.getId() === id);
+    }
+
+    public readonly add = (numericQuestion: NumericQuestion<ID>): CompositionQuestion<ID> => {
+        this.questions.push(numericQuestion);
+        return this;
+    }
+
+    public readonly addAll = (...questions: Array<NumericQuestion<ID>>): QuestionCollection<ID> => {
+        questions.forEach((question) => this.add(question));
+        return this;
+    }
+
+    public readonly handleNumericQuestions = (handler: (numericQuestion: NumericQuestion<ID>) => void): void => {
+        this.questions
+            .forEach(question => handler(question));
+    }
+
+    public readonly getAnswer = (): number | undefined => this.answer;
+
+    public readonly setAnswer = (answer: number): void => {
+        this.answer = answer;
+    }
+
+    public readonly sumsUp = (): boolean => {
+        return this.answer ? this.questions
+            .map(question => question.getAnswer())
+            .reduce((answer1, answer2) => answer1 + answer2) === this.answer :
+            false;
+    }
+}
