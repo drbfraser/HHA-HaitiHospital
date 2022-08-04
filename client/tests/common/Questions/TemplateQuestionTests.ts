bdd94e5cb69a6cec0b1861f615fc7eb6d44cf191
Daniel Maia <dgomesma@sfu.ca>
2022-08-04 15:30:42 -0700

    Further abstract testing

--- client/tests/common/Questions/TemplateQuestionTests.ts
+++ client/tests/common/Questions/TemplateQuestionTests.ts
@@ -1,15 +1,15 @@
-import { expect } from 'chai';
 import { QuestionItem } from '../../../src/common/Questions/QuestionItem';
+import { testSetAndGetHOF } from '../../testUtils';
 
 export const IdTest = <ID, Question extends QuestionItem<ID>>(
   id: ID,
   questionCreator: (id: ID) => Question,
-) => {
-  return it('Should be able to set and get question ID', function () {
-    // Arrange/Act
-    let question: Question = questionCreator(id);
-
-    // Assert
-    expect(question.getId()).to.be.equal(id);
+): void => {
+  testSetAndGetHOF({
+    testName: `Should be able to create and get question ID`,
+    setter: questionCreator,
+    getter: (question) => question.getId(),
+    mapping: (id) => id,
+    prop: id,
   });
 };
---
+++ client/tests/testUtils.ts
@@ -0,0 +1,21 @@
+import { expect } from 'chai';
+
+interface TestSetAndGetHOFArgs<Property, Obj> {
+  testName: string,
+  setter: (prop: Property) => Obj;
+  getter: (obj: Obj) => Property;
+  mapping: (prop: Property) => Property;
+  prop: Property;
+}
+
+export const testSetAndGetHOF = <Property, Obj>(
+    args: TestSetAndGetHOFArgs<Property, Obj>,
+): void => {
+    it(args.testName, function (){
+        // Arrange/Act
+        let object: Obj = args.setter(args.prop);
+
+        // Assert
+        expect(args.getter(object)).to.be.equal(args.mapping(args.prop));
+    }
+};
