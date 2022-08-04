a8d626023f0926ad29e50f9e174da587a639a373
Long K. Tran <kltran@sfu.ca>
2022-08-04 00:11:38 -0700

    remove unhelpful reusable express validation chain

--- server/src/sanitization/schemas/biomech.ts
+++ server/src/sanitization/schemas/biomech.ts
@@ -1,18 +1,22 @@
-import { body } from 'express-validator';
+import { msgString } from 'sanitization/messages';
+import { isImage, MUST_BE_AN_IMAGE_FILE } from 'sanitization/validators/isImage';
 import { INVALID_BIOMECH_PRIORITY, isBiomechPriority } from 'sanitization/validators/is_biomech_priority';
-import { isImage, MUST_BE_AN_IMAGE_FILE } from '../validators/isImage';
-import { isString, nonFalsy } from './common';
+import { nonFalsyBodyField } from './common';
 
 const bioMechCreate = [
-  isString(nonFalsy(body(`equipmentName`))),
-  isString(nonFalsy(body(`equipmentFault`))),
-  isString(nonFalsy(body(`equipmentPriority`)))
-    .custom(isBiomechPriority)
-    .withMessage(INVALID_BIOMECH_PRIORITY),
-  nonFalsy(body(`file`))
+  nonFalsyBodyField(`equipmentName`)
+    .isString().withMessage(msgString)
+    .trim().escape(),
+  nonFalsyBodyField(`equipmentFault`)
+    .isString().withMessage(msgString)
+    .trim().escape(),
+  nonFalsyBodyField(`equipmentPriority`)
+    .isString().withMessage(msgString)
+    .trim().escape()
+    .custom(isBiomechPriority).withMessage(INVALID_BIOMECH_PRIORITY),
+  nonFalsyBodyField(`file`)
     .isObject()
-    .custom(isImage)
-    .withMessage(MUST_BE_AN_IMAGE_FILE)
+    .custom(isImage).withMessage(MUST_BE_AN_IMAGE_FILE)
 ];
 
 export const Biomech = {
--- server/src/sanitization/schemas/common.ts
+++ server/src/sanitization/schemas/common.ts
@@ -1,17 +1,12 @@
-import { ValidationChain } from 'express-validator';
+import { body, ValidationChain } from 'express-validator';
 
-// Field must be non-falsy (not empty, not undefined, not null) for the chain to persit.
-export const nonFalsy = (chain: ValidationChain): ValidationChain => {
-  return chain
+// Field in body must be non-falsy (not empty, not undefined, not null) for the chain to persit.
+export const nonFalsyBodyField = (field: string): ValidationChain => {
+  return body(field)
     .exists({
       checkFalsy: true,
       checkNull: true
     })
     .withMessage(`Expecting a value`)
     .if((value) => value);
-};
-
-// Field must be a string. If so, trim whitespace on 2 ends and escape string.
-export const isString = (chain: ValidationChain): ValidationChain => {
-  return chain.isString().withMessage(`Expecting text`).trim().escape();
 };
