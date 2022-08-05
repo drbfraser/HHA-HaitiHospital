efddf2e771cbd1bf11398c5ab74dd2151ea58af2
Daniel Maia <dgomesma@sfu.ca>
2022-08-04 20:52:15 -0700

    Add better support for validation

--- client/src/common/Questions/Question.ts
+++ client/src/common/Questions/Question.ts
@@ -1,42 +1,45 @@
-import { QuestionItem } from "./QuestionItem";
+import { QuestionItem } from './QuestionItem';
 
 export interface ValidationResult<ErrorType> {
-    readonly error: ErrorType;
-    readonly message?: string;
+  readonly isValid: boolean;
+  readonly error?: ErrorType;
+  readonly message?: string;
 }
 
 export abstract class Question<ID, T> extends QuestionItem<ID> {
-    private readonly prompt: string;
-    private answer?: T;
+  private readonly prompt: string;
+  private answer?: T;
 
-    private readonly validators: Array<(answer?: T) => ValidationResult<unknown>>;
-    
-    constructor(
-        id: ID,
-        prompt: string,
-        defaultAnswer?: T,
-    ) {
-        super(id);
-        this.prompt = prompt;
-        this.answer = defaultAnswer;
-        this.validators = new Array<(answer: T) => ValidationResult<unknown>>();
-    }
+  private readonly validators: Array<(answer?: T) => ValidationResult<unknown>>;
 
-    public readonly getPrompt = (): string => this.prompt;
+  constructor(id: ID, prompt: string, defaultAnswer?: T) {
+    super(id);
+    this.prompt = prompt;
+    this.answer = defaultAnswer;
+    this.validators = new Array<(answer: T) => ValidationResult<unknown>>();
+  }
 
-    public readonly getAnswer = (): T | undefined => this.answer;
+  public readonly getPrompt = (): string => this.prompt;
 
-    public readonly setAnswer = (answer: T): void => {
-        this.answer = answer;
-    }
+  public readonly getAnswer = (): T | undefined => this.answer;
 
-    public readonly addValidator = (validator: (answer?: T) => ValidationResult<unknown>): void => {
-        this.validators.push(validator);
-    }
+  public readonly setAnswer = (answer: T): void => {
+    this.answer = answer;
+  };
 
-    public readonly validate = (): Array<ValidationResult<unknown>> => {
-        return this.validators
-            .map((validator: (answer?: T) => ValidationResult<unknown>) => validator(this.answer));
-    }
+  public readonly addValidator = (validator: (answer?: T) => ValidationResult<unknown>): void => {
+    this.validators.push(validator);
+  };
 
+  public readonly isValid = (): boolean => {
+    return this.validators
+      .map((validator) => validator(this.answer).isValid)
+      .reduce((isValid1, isValid2) => isValid1 && isValid2);
+  };
+
+  public readonly getValidationResults = (): Array<ValidationResult<unknown>> => {
+    return this.validators.map((validator: (answer?: T) => ValidationResult<unknown>) =>
+      validator(this.answer),
+    );
+  };
 }
