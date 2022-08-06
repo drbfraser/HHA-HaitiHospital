f40e13c7e7392568b1a8d5afc2a88a3379f5961d
Daniel Maia <dgomesma@sfu.ca>
2022-08-05 23:54:17 -0700

    Add text question tests

--- client/tests/common/Questions/SimpleQuestionTypes.spec.ts
+++ client/tests/common/Questions/SimpleQuestionTypes.spec.ts
@@ -1,4 +1,5 @@
 import { expect } from 'chai';
+import { deserialize } from 'v8';
 import { number, string } from 'yup';
 import { Question } from '../../../src/common/Questions/Question';
 import { NumericQuestion, TextQuestion } from '../../../src/common/Questions/SimpleQuestionTypes';
@@ -57,5 +58,39 @@ describe('SimpleQuestions', function () {
     const DEFAULT_ID: number = 3;
     const DEFAULT_PROMPT: string = "What's the magic word?";
     const DEFAULT_ANSWER: string = 'sudo';
+
+    simpleQuestionDefaultTests<number, string, string>({
+      idTestArgs: {
+        id: DEFAULT_ID,
+        questionCreator: (id) => new TextQuestion(id, DEFAULT_PROMPT),
+      },
+      promptTestArgs: {
+        prompt: DEFAULT_PROMPT,
+        questionCreator: (prompt) => new TextQuestion(DEFAULT_ID, prompt),
+      },
+      answerTestArgs: {
+        answer: DEFAULT_ANSWER,
+        questionCreator: () => new TextQuestion(DEFAULT_ID, DEFAULT_PROMPT),
+      },
+      validationTestArgs: {
+        sampleAnswer: DEFAULT_ANSWER,
+        questionCreator: () => new TextQuestion(DEFAULT_ID, DEFAULT_PROMPT),
+        getDefaultErrorType: (number) => `ErrorType${number}`,
+        getDefaultErrorMessage: (number) => `Error Message ${number}`,
+      },
+      serializableQuestionTestArgs: {
+        questionArranger: () => {
+          let question = new TextQuestion(DEFAULT_ID, DEFAULT_PROMPT);
+          question.setAnswer(DEFAULT_ANSWER);
+          return question;
+        },
+        expectations: [
+          (deserialized) => expect(deserialized).to.be.instanceof(TextQuestion),
+          (deserialized) => expect(deserialized.getId()).to.be.equal(DEFAULT_ID),
+          (deserialized) => expect(deserialized.getPrompt()).to.be.equal(DEFAULT_PROMPT),
+          (deserialized) => expect(deserialized.getAnswer()).to.be.equal(DEFAULT_ANSWER),
+        ],
+      },
+    });
   });
 });
