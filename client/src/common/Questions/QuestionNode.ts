36dba78b208ec28c72690219c07c060cdbc44ad1
Daniel Maia <dgomesma@sfu.ca>
2022-09-20 15:00:41 -0700

    Remove unused import

--- client/src/common/Questions/QuestionNode.ts
+++ client/src/common/Questions/QuestionNode.ts
@@ -4,8 +4,6 @@
     Its type is meant to be further specialized to determine the kind of node it 
     represents and what other capabilities it should support.
 */
-import { serializable } from 'common/Serializer/ObjectSerializer';
-
 export abstract class QuestionNode<ID, ErrorType> {
   private readonly id: ID;
 
