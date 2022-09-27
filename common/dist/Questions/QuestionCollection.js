d92feae29d164264b470575755802258fdc8a86e
Zwe Min Soe <zsoe@Zwes-MacBook-Pro.local>
2022-09-27 13:29:40 -0700

    ignore dist folder

--- common/dist/Questions/QuestionCollection.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.QuestionCollection = void 0;
-const QuestionItem_1 = require("./QuestionItem");
-class QuestionCollection extends QuestionItem_1.QuestionItem {
-}
-exports.QuestionCollection = QuestionCollection;
--- common/dist/validation_json.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-// JSON for reporting errors across the stack.
-Object.defineProperty(exports, "__esModule", { value: true });
