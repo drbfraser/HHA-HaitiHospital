922dd65f3551017ea43e5585f84e19346a26658e
Zwe Min Soe <zsoe@Zwes-MacBook-Pro.local>
2022-10-09 12:21:48 -0700

    separate update-common script

--- scripts/update_common.sh
+++ scripts/update_common.sh
@@ -1,17 +1,11 @@
 #!/bin/sh
 
 cd ../common
+npm install
 npm run build
 
-cd ../client
+cd ../${1}
 npm uninstall @hha/common
 npm pack ../common
 npm install hha-common-1.0.0.tgz
-
-cd ../server
-npm uninstall @hha/common
-npm pack ../common
-npm install hha-common-1.0.0.tgz
-
-cd ..
 
