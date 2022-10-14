3c083031c9b23a2fd8837f6ded318735709d018c
Kiaan Castillo <kiaan_castillo@sfu.ca>
2022-10-13 21:16:12 -0700

    Finish rehab questions up to question 13

--- common/tests/Questions/Report.spec.ts
+++ common/tests/Questions/Report.spec.ts
@@ -1,4 +1,9 @@
-import { CompositionQuestion, QuestionGroup, NumericQuestion, QuestionNode, ExpandableQuestion, SingleSelectionQuestion, MultipleSelectionQuestion } from '../../src';
+import { CompositionQuestion, QuestionGroup, NumericQuestion, QuestionNode, ExpandableQuestion, SingleSelectionQuestion, MultipleSelectionQuestion, TextQuestion } from '../../src';
+
+const questionIdGeneratorBuilder =
+  (questionId: string) =>
+  (questionGroupIndex: number): string =>
+    `${questionId}_${questionGroupIndex}`;
 
 const buildRehabMockReport = () => {
   const reportID: string = 'rehab-report_1';
@@ -11,8 +16,7 @@ const buildRehabMockReport = () => {
   const q4: NumericQuestion<string, string> = new NumericQuestion<string, string>('4', 'Hospitalized', 17);
 
   // Question 5
-  const q5IdGenerator = (questionGroupIndex: number): string => `5_${questionGroupIndex}`;
-  const q5: ExpandableQuestion<string, string> = new ExpandableQuestion<string, string>('5', q5IdGenerator);
+  const q5: ExpandableQuestion<string, string> = new ExpandableQuestion<string, string>('5', questionIdGeneratorBuilder('5'));
 
   const q5_1: SingleSelectionQuestion<string, string> = new SingleSelectionQuestion<string, string>('5_1', 'Discharged diagnosis', 0);
   const q5_1_choices: string[] = ['SCI', 'Stroke', 'Other'];
@@ -59,6 +63,84 @@ const buildRehabMockReport = () => {
   }
 
   q5.addAllToTemplate(q5_1, q5_2, q5_3, q5_4, q5_5, q5_6, q5_7, q5_8);
+
+  // Question 6
+  const q6: ExpandableQuestion<string, string> = new ExpandableQuestion<string, string>('6', questionIdGeneratorBuilder('6'));
+  const q6_1: SingleSelectionQuestion<string, string> = new SingleSelectionQuestion<string, string>('6_1', 'Diagnosis');
+  const q6_1_choices: string[] = ['SCI', 'CVA', 'Other'];
+  for (const choice in q6_1_choices) {
+    q6_1.addChoice(choice);
+  }
+
+  const q6_2: NumericQuestion<string, string> = new NumericQuestion<string, string>('6_2', 'Age');
+
+  const q6_3: SingleSelectionQuestion<string, string> = new SingleSelectionQuestion<string, string>('6_3', 'Cause of death');
+  const q6_3_choices: string[] = ['Suspected CVA'];
+  for (const choice in q6_3_choices) {
+    q6_3.addChoice(choice);
+  }
+
+  q6.addAllToTemplate(q6_1, q6_2, q6_3);
+
+  // Question 7
+  const q7: ExpandableQuestion<string, string> = new ExpandableQuestion<string, string>('7', questionIdGeneratorBuilder('7'));
+  const q7_1: SingleSelectionQuestion<string, string> = new SingleSelectionQuestion<string, string>('7_1', 'Diagnosis');
+  const q7_1_choices: string[] = ['SCI', 'CVA', 'Other'];
+  for (const choice in q7_1_choices) {
+    q7_1.addChoice(choice);
+  }
+
+  const q7_2: NumericQuestion<string, string> = new NumericQuestion<string, string>('7_2', 'Age');
+  const q7_3: TextQuestion<string, string> = new TextQuestion<string, string>('7_3', 'Cause of death');
+
+  q7.addAllToTemplate(q7_1, q7_2, q7_3);
+
+  // Questions 8 to 10
+  const q8: NumericQuestion<string, string> = new NumericQuestion<string, string>('8', 'Days hospitalised');
+  const q9: NumericQuestion<string, string> = new NumericQuestion<string, string>('9', 'Referrals');
+  const q10: NumericQuestion<string, string> = new NumericQuestion<string, string>('10', 'Transfers');
+
+  // Question 11
+  const q11: NumericQuestion<string, string> = new NumericQuestion<string, string>('11', 'Self-discharged');
+  const q11_1: SingleSelectionQuestion<string, string> = new SingleSelectionQuestion<string, string>('11_1', 'Reason for self-discharge');
+  const q11_1_choices: string[] = ['Finance: Left as cannot afford care', 'Finance: Left to avoid paying', 'Religious/Cultural', 'Personal/Family', 'Other'];
+  for (const choice in q11_1_choices) {
+    q11_1.addChoice(choice);
+  }
+
+  // Question 12
+  const q12: CompositionQuestion<string, string> = new CompositionQuestion<string, string>('12');
+  const q12_1: QuestionGroup<string, string> = new QuestionGroup<string, string>('12_1');
+  const q12_1_1: NumericQuestion<string, string> = new NumericQuestion<string, string>('12_1_1', 'Not ready from therapy standpoint');
+  const q12_1_2: NumericQuestion<string, string> = new NumericQuestion<string, string>('12_1_2', 'Wound Care');
+  const q12_1_3: NumericQuestion<string, string> = new NumericQuestion<string, string>('12_1_3', 'Other medical reason (such as IV medication)');
+  const q12_1_4: NumericQuestion<string, string> = new NumericQuestion<string, string>('12_1_4', 'Financial/no place to discharge to');
+
+  q12_1.addAll(q12_1_1, q12_1_2, q12_1_3);
+
+  const q12_2: QuestionGroup<string, string> = new QuestionGroup<string, string>('12_2');
+  const q12_2_1: NumericQuestion<string, string> = new NumericQuestion<string, string>('12_2_1', '1-3 months');
+  const q12_2_2: NumericQuestion<string, string> = new NumericQuestion<string, string>('12_2_2', '3-6 months');
+  const q12_2_3: NumericQuestion<string, string> = new NumericQuestion<string, string>('12_2_3', '6 months - 1 year');
+  const q12_2_4: NumericQuestion<string, string> = new NumericQuestion<string, string>('12_2_4', '1-2 years');
+  const q12_2_5: NumericQuestion<string, string> = new NumericQuestion<string, string>('12_2_5', '2-3 years ');
+  const q12_2_6: NumericQuestion<string, string> = new NumericQuestion<string, string>('12_2_6', '3+ years ');
+
+  q12_2.addAll(q12_2_1, q12_2_2, q12_2_3, q12_2_4, q12_2_5, q12_2_6);
+  // TODO: Add q12_1 and q12_2 to q12
+
+  // Question 13
+  const q13: CompositionQuestion<string, string> = new CompositionQuestion<string, string>('13');
+  const q13_1: QuestionGroup<string, string> = new QuestionGroup<string, string>('13_1');
+  const q13_1_1: NumericQuestion<string, string> = new NumericQuestion<string, string>('13_1_1', 'Quarter Morin');
+  const q13_1_2: NumericQuestion<string, string> = new NumericQuestion<string, string>('13_1_2', 'Cap Haitian');
+  const q13_1_3: NumericQuestion<string, string> = new NumericQuestion<string, string>('13_1_3', 'Department Nord');
+  const q13_1_4: NumericQuestion<string, string> = new NumericQuestion<string, string>('13_1_4', 'Other departments');
+
+  q13_1.addAll(q13_1_1, q13_1_2, q13_1_3, q13_1_4);
+  // TODO: Add q13_1 to q13
+
+  rehabReport.addAll(q1, q2, q3, q4, q5, q6, q7, q8, q9, q10, q11, q12, q13);
 };
 
 describe('Report', function () {
