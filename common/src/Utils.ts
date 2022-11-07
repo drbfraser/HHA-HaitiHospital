535c1ef7730edfc6ecd967822a98d1b030192e22
Daniel Gomes Maia Filho <dg.maia01@gmail.com>
2022-11-06 20:46:27 -0800

    Remove unnecessary function

--- common/src/Utils.ts
+++ common/src/Utils.ts
@@ -14,18 +14,3 @@ export const recursiveConsumeObjectHOF =
         consumer(object);
     }
 }
-
-export function recursiveMapObjectHOF<T>(mapper: (something: any) => T, defaultValue: T): (object:any) => T {
-    return (object: any): T => {
-        if (!(object instanceof Object) || object instanceof Function) {
-            return defaultValue;
-        }
-        
-        Object.entries(object)
-            .forEach(([key, value]) => {
-                recursiveConsumeObjectHOF(mapper)(value); 
-        });
-        
-        return mapper(object);
-    }
-}
