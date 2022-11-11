ab77400e487f6b7f397b11943c59a17a7e8add0c
Zwe Min Soe <zsoe@Zwes-MacBook-Pro.local>
2022-11-10 22:59:16 -0800

    added unit test

--- server/src/routes/api/report.ts
+++ server/src/routes/api/report.ts
@@ -1,26 +1,27 @@
+import { ObjectSerializer } from '@hha/common';
 import { NextFunction, Response } from 'express';
 import { RequestWithUser } from 'utils/definitions/express';
 import { HTTP_CREATED_CODE } from '../../exceptions/httpException';
 import requireJwtAuth from '../../middleware/requireJwtAuth';
-
 import { ReportCollection } from '../../models/report';
 
 const router = require('express').Router();
 
 //Save report
-router.route('/save').put(requireJwtAuth, async (req: RequestWithUser, res: Response, next: NextFunction) => {
+router.route('/').post(requireJwtAuth, async (req: RequestWithUser, res: Response, next: NextFunction) => {
   try {
     const { departmentId, reportMonth, submittedUserId, serializedReport } = req.body;
-    // NOTE: JSON.parse keeps the __class__ property
-    const reportObject = JSON.parse(serializedReport);
+    const objectSerializer = new ObjectSerializer();
+    const reportObject = objectSerializer.deserialize(serializedReport);
+    // NOTE: May need to sanitize the reportObject before saving
     const newReport = new ReportCollection({
       departmentId,
-      reportMonth,
       submittedUserId,
+      reportMonth,
       reportObject
     });
-    await newReport.save();
-    return res.status(HTTP_CREATED_CODE).json({ message: 'Report saved' });
+    const saved = await newReport.save();
+    return res.status(HTTP_CREATED_CODE).json({ message: 'Report saved', report: saved });
   } catch (e) {
     console.log(e);
     next(e);
---
+++ server/tests/unitTests/report.spec.ts
@@ -0,0 +1,63 @@
+import { QuestionGroup } from '@hha/common';
+import { ObjectSerializer, buildRehabMockReport } from '@hha/common';
+import http from 'http';
+import { Application } from 'express';
+import { setupApp, setupHttpServer, Accounts, closeServer } from './testTools/mochaHooks';
+import { CSRF_ENDPOINT, LOGIN_ENDPOINT, SAVE_REPORT_ENDPOINT } from './testTools/endPoints';
+
+const expect = require('chai').expect;
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+chai.use(chaiHttp);
+
+let httpServer: http.Server;
+let agent: any;
+let csrf: String;
+
+describe('report tests', function () {
+  before('Create a Working Server and Login With Admin', function (done) {
+    let app: Application = setupApp();
+    httpServer = setupHttpServer(app);
+    agent = chai.request.agent(app);
+
+    agent.get(CSRF_ENDPOINT).end(function (error, res) {
+      if (error) done(error);
+      csrf = res?.body?.CSRFToken;
+
+      agent
+        .post(LOGIN_ENDPOINT)
+        .set({ 'Content-Type': 'application/json', 'CSRF-Token': csrf })
+        .send(Accounts.AdminUser)
+        .end(function (error: any, response: any) {
+          if (error) return done(error);
+          done();
+        });
+    });
+  });
+
+  after('Close a Working Server', function () {
+    closeServer(agent, httpServer);
+  });
+
+  it('should save report correctly', function (done) {
+    const objectSerializer = new ObjectSerializer();
+    const serializedReport = objectSerializer.serialize(buildRehabMockReport());
+    agent
+      .post(SAVE_REPORT_ENDPOINT)
+      .set({ 'Content-Type': 'application/json', 'CSRF-Token': csrf })
+      .send({
+        departmentId: '6324f6be9ff2276b8f5647d1',
+        submittedUserId: '6324f6be9ff2276b8f5647e5',
+        reportMonth: new Date(),
+        serializedReport
+      })
+      .end(function (err: any, res: any) {
+        expect(err).to.be.null;
+        expect(res).to.have.status(201);
+        expect(res.body).to.have.property('message');
+        expect(res.body).to.have.property('report');
+        expect(new QuestionGroup<string, string>('ROOT', res.body.report.reportObject) instanceof QuestionGroup).to.be.true;
+        done();
+      });
+  });
+});
