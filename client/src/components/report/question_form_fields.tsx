a39bb938f8106d7b2b35de23e0405f7355efc224
Kiaan Castillo <kiaan_castillo@sfu.ca>
2022-11-13 19:03:59 -0800

    Use pattern matching to handle question rendering

--- client/src/components/report/question_form_fields.tsx
+++ client/src/components/report/question_form_fields.tsx
@@ -1,17 +1,20 @@
-import {  QuestionNode, NumericQuestion, ExpandableQuestion, TextQuestion } from "@hha/common"
+import {  QuestionNode, NumericQuestion, ExpandableQuestion, TextQuestion, CompositionQuestion, MultipleSelectionQuestion, SingleSelectionQuestion, QuestionGroup } from "@hha/common"
 import { useState } from 'react';
 import './styles.css';
 
-export const buildQuestionFormField = (question: QuestionNode): JSX.Element => {
-  if (question instanceof NumericQuestion) {
-    return <NumericQuestionFormField key={question.id} question={question} />
-  } else if (question instanceof ExpandableQuestion) {
-    return <ExpandableQuestionFormField key={question.id} question={question} />
-  } else if (question instanceof TextQuestion) {
-    return <TextQuestionFormField key={question.id} question={question} />
-  } else {
-    return <p key={question.id}>(WIP) Non-supported question type</p>
-  }
+interface QuestionFormFieldHandlerEntry<ID, ErrorType> {
+  readonly className: string;
+  readonly FormFieldComponent: (question: QuestionNode<ID, ErrorType>) => JSX.Element;
+}
+
+interface QuestionFormFieldHandlerMap<ID, ErrorType> {
+  readonly textQuestion: QuestionFormFieldHandlerEntry<ID, ErrorType>;
+  readonly numericQuestion: QuestionFormFieldHandlerEntry<ID, ErrorType>;
+  readonly singleSelectionQuestion: QuestionFormFieldHandlerEntry<ID, ErrorType>;
+  readonly multipleSelectionQuestion: QuestionFormFieldHandlerEntry<ID, ErrorType>;
+  readonly questionGroup: QuestionFormFieldHandlerEntry<ID, ErrorType>;
+  readonly compositionQuestion: QuestionFormFieldHandlerEntry<ID, ErrorType>;
+  readonly expandableQuestion: QuestionFormFieldHandlerEntry<ID, ErrorType>;
 }
 
 const FormField = ({children}): JSX.Element => {
@@ -24,6 +27,13 @@ const FormFieldLabel = ({id, prompt}): JSX.Element => {
   const orderedLabel = id.replaceAll('_', '.')
 
   return <label htmlFor={id} className='form-label'>{orderedLabel}. {prompt}</label>
+}
+
+const PlaceholderFormField = ({question}: NumericQuestion): JSX.Element => {
+  return <FormField>
+    <FormFieldLabel id={question.id} prompt={question.prompt} />
+    <p>(WIP) Non-supported question type</p>
+  </FormField>
 }
 
 const NumericQuestionFormField = ({question}: NumericQuestion): JSX.Element => {
@@ -68,4 +78,41 @@ const ExpandableQuestionFormField = ({question}: ExpandableQuestion): JSX.Elemen
       })}
     </div>
   </FormField>
+}
+
+const questionFormFieldMapper: QuestionFormFieldHandlerMap<string, string> = {
+  textQuestion: {
+    className: TextQuestion.name,
+    FormFieldComponent: TextQuestionFormField
+  },
+  numericQuestion: {
+    className: NumericQuestion.name,
+    FormFieldComponent: NumericQuestionFormField
+  },
+  singleSelectionQuestion: {
+    className: SingleSelectionQuestion.name,
+    FormFieldComponent: PlaceholderFormField
+  },
+  multipleSelectionQuestion: {
+    className: MultipleSelectionQuestion.name,
+    FormFieldComponent: PlaceholderFormField
+  },
+  questionGroup: {
+    className: QuestionGroup.name,
+    FormFieldComponent: PlaceholderFormField
+  },
+  compositionQuestion: {
+    className: CompositionQuestion.name,
+    FormFieldComponent: PlaceholderFormField
+  },
+  expandableQuestion: {
+    className: ExpandableQuestion.name,
+    FormFieldComponent: ExpandableQuestionFormField
+  },
+}
+
+export const buildQuestionFormField = (question: QuestionNode): JSX.Element => {
+  const { className, FormFieldComponent } = Object.values(questionFormFieldMapper).find((classNameMap) => classNameMap.className === question.constructor.name);
+
+  return <FormFieldComponent key={question.id} question={question} />
 }
