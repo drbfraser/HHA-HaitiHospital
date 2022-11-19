3d73e4d25825847207aee311fbd8d8817717b101
Daniel G. Maia Filho <dgomesma@sfu.ca>
2022-11-18 17:22:59 -0800

    Temporary fixes on front-end

--- client/src/components/report/question_form_fields.tsx
+++ client/src/components/report/question_form_fields.tsx
@@ -17,6 +17,10 @@ interface QuestionFormFieldHandlerMap<ID, ErrorType> {
   readonly expandableQuestion: QuestionFormFieldHandlerEntry<ID, ErrorType>;
 }
 
+// Temporary placeholders
+type ID = string;
+type ErrorType = string;
+
 const FormField = ({children}): JSX.Element => {
   return <fieldset className="mb-3">
     {children}
@@ -29,107 +33,55 @@ const FormFieldLabel = ({id, prompt}): JSX.Element => {
   return <label htmlFor={id} className='form-label'>{orderedLabel}. {prompt}</label>
 }
 
-const PlaceholderFormField = ({question}: NumericQuestion): JSX.Element => {
+const PlaceholderFormField = (question: NumericQuestion<ID, ErrorType>): JSX.Element => {
   return <FormField>
-    <FormFieldLabel id={question.id} prompt={question.prompt} />
+    <FormFieldLabel id={question.getId()} prompt={question.getPrompt()} />
     <p>(WIP) Non-supported question type</p>
   </FormField>
 }
 
-const NumericQuestionFormField = ({question}: NumericQuestion): JSX.Element => {
+const NumericQuestionFormField = (question: NumericQuestion<ID, ErrorType>): JSX.Element => {
   return <FormField>
-    <FormFieldLabel id={question.id} prompt={question.prompt} />
+    <FormFieldLabel id={question.getId()} prompt={question.getPrompt()} />
     <input
       className="form-control w-fit"
       type="number"
       min="0"
-      defaultValue={question.answer}
+      defaultValue={question.getAnswer()}
     />
   </FormField>
 }
 
-const TextQuestionFormField = ({question}: TextQuestion): JSX.Element => {
+const TextQuestionFormField = (question: TextQuestion<ID, ErrorType>): JSX.Element => {
+
   return <FormField>
-    <FormFieldLabel id={question.id} prompt={question.prompt} />
+    <FormFieldLabel id={question.getId()} prompt={question.getPrompt()} />
     <input
       className="form-control w-fit"
       type="text"
-      defaultValue={question.answer}
+      defaultValue={question.getAnswer()}
     />
   </FormField>
 }
 
 
-const ExpandableQuestionFormField = ({question}: ExpandableQuestion): JSX.Element => {
+const ExpandableQuestionFormField = (question: ExpandableQuestion<ID, ErrorType>): JSX.Element => {
   const [numberOfItems, setNumberOfItems] = useState(0)
 
   return <FormField>
-    <FormFieldLabel id={question.id} prompt={question.prompt} />
+    <FormFieldLabel id={question.getId()} prompt={question.getPrompt()} />
     <input className='col-sm form-control w-fit' type='number' min="0" defaultValue={numberOfItems} onChange={(e) => setNumberOfItems(parseInt(e.target.value))} />
     <div>
       {Array.from({length: numberOfItems}, (_, index) => {
         return (
           <fieldset key={index} className='mt-3'>
             <h6 className='uppercase text-lg'>Item {index + 1}</h6>
-            {question.questionsTemplate.questionItems.map((q) => buildQuestionFormField(q))}
+            {/* Fix in a follow-up MR */}
+            {/* question.questionsTemplate.questionItems.map((q) => buildQuestionFormField(q)) */}
           </fieldset>
         )
       })}
     </div>
-  </FormField>
-}
-
-const SingleSelectionQuestionFormField = ({question}: SingleSelectionQuestion) => {
-  const [currentSelection, setCurrentSelection] = useState(question.answer || 0)
-
-  return <FormField>
-    <FormFieldLabel id={question.id} prompt={question.prompt} />
-    {question.choices.map(({chosen, description}, index) =>
-      <div key={`${question.id}-${index}`}>
-        <input 
-          id={`${question.id}-${index}`} 
-          className='form-check-input' 
-          name={question.prompt} 
-          type="radio" 
-          checked={index === currentSelection} 
-          onChange={() => {
-            question.setAnswer(index)
-            setCurrentSelection(index)}
-          } 
-        /> 
-        &nbsp;<label htmlFor={`${question.id}-${index}`}>{description}</label>
-      </div>
-    )}
-  </FormField>
-}
-
-const MultiSelectionQuestionFormField = ({question}: SingleSelectionQuestion) => {
-  const [currentSelections, setCurrentSelections] = useState(question.answer || [])
-
-  return <FormField>
-    <FormFieldLabel id={question.id} prompt={question.prompt} />
-    {question.choices.map(({chosen, description}, index) =>
-      <div key={`${question.id}-${index}`}>
-        <input 
-          id={`${question.id}-${index}`} 
-          className='form-check-input' 
-          name={question.prompt} 
-          type="checkbox" 
-          checked={currentSelections.includes(index)} 
-          onChange={() => {
-            let newSelections = []
-            if (currentSelections.includes(index)) {
-              newSelections = currentSelections.filter(s => s !== index)
-            } else {
-              newSelections = [...currentSelections, index]
-            }
-            question.setAnswer(newSelections)
-            setCurrentSelections(newSelections)
-          }} 
-        /> 
-        &nbsp;<label htmlFor={`${question.id}-${index}`}>{description}</label>
-      </div>
-    )}
   </FormField>
 }
 
@@ -144,11 +96,11 @@ const questionFormFieldMapper: QuestionFormFieldHandlerMap<string, string> = {
   },
   singleSelectionQuestion: {
     className: SingleSelectionQuestion.name,
-    FormFieldComponent: SingleSelectionQuestionFormField
+    FormFieldComponent: PlaceholderFormField
   },
   multipleSelectionQuestion: {
     className: MultipleSelectionQuestion.name,
-    FormFieldComponent: MultiSelectionQuestionFormField
+    FormFieldComponent: PlaceholderFormField
   },
   questionGroup: {
     className: QuestionGroup.name,
@@ -164,8 +116,8 @@ const questionFormFieldMapper: QuestionFormFieldHandlerMap<string, string> = {
   },
 }
 
-export const buildQuestionFormField = (question: QuestionNode): JSX.Element => {
+export const buildQuestionFormField = (question: QuestionNode<ID, ErrorType>): JSX.Element => {
   const { className, FormFieldComponent } = Object.values(questionFormFieldMapper).find((classNameMap) => classNameMap.className === question.constructor.name);
 
-  return <FormFieldComponent key={question.id} question={question} />
+  return <FormFieldComponent key={question.getId()} question={question} />
 }
