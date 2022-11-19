e295e60a775223d881da180ec626350f76294e95
Allan Che <aca193@sfu.ca>
2022-11-19 01:49:53 -0800

    Added Navigation Tests

--- client/cypress/e2e/home.cy.ts
+++ client/cypress/e2e/home.cy.ts
@@ -4,13 +4,26 @@ import { LoginPage } from '../support/pages/LoginPage';
 import en from '../../src/locales/en/translationEN.json';
 import { HomePage } from '../support/pages/HomePage';
 
-describe('Login tests', function () {
+describe('Home Page Tests', function () {
+  const loginPage = new LoginPage();
   const homePage = new HomePage();
 
-  beforeEach('Go to page', function () {
-    homePage.visit();
-    cy.contains(en.signInSignIn).should('be.visible');
+  const username = Cypress.env('username');
+  const password = Cypress.env('password');
+
+  beforeEach('Logging in...', function () {
+    loginPage.visit();
+    loginPage.usernameInput(username).passwordInput(password).clickLoginButton();
+    cy.url().should('include', '/home');
   });
 
-  it('should login successfully', function () {});
+  it('Should Navigate to the Leaderboard Page', function () {
+    homePage.clickLeaderboardPageButton();
+    cy.url().should('include', '/leaderBoard');
+  });
+
+  it('Should Navigate to the Messages Page', function () {
+    homePage.clickSeeMoreMessagesButton();
+    cy.url().should('include', '/message-board');
+  });
 });
