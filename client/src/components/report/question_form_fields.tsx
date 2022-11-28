3bd17488ce4769d17fa8b5a7f645318cb418fabf
Kiaan Castillo <kiaan_castillo@sfu.ca>
2022-11-28 14:45:00 -0800

    Revert "Fix expandable question nested items conflicting with each other" commit to use a simpler solution
    This reverts commit 386f6d9bb837ef728ed6ad3cbe002111353f0fe5.

--- client/src/components/report/question_form_fields.tsx
+++ client/src/components/report/question_form_fields.tsx
@@ -1,189 +1,140 @@
-import {
-  QuestionNode,
-  NumericQuestion,
-  ExpandableQuestion,
-  TextQuestion,
-  CompositionQuestion,
-  MultipleSelectionQuestion,
-  SingleSelectionQuestion,
-  QuestionGroup,
-  ImmutableChoice,
-} from '@hha/common';
+import {  QuestionNode, NumericQuestion, ExpandableQuestion, TextQuestion, CompositionQuestion, MultipleSelectionQuestion, SingleSelectionQuestion, QuestionGroup, ImmutableChoice } from "@hha/common"
 import { useState } from 'react';
-import { v4 as uuid } from 'uuid';
 import './styles.css';
 
-type FunctionalComponent = (object: Object, id: ID) => JSX.Element;
+type FunctionalComponent = (object: Object) => JSX.Element;
 
 // Temporary placeholders
 // TODO: Decide on an appropriate types for those
 type ID = string;
-type ErrorType = string;
-interface FormFieldProps {
-  question: any;
-  id: string;
+type ErrorType = string; 
+
+const FormField = ({children}): JSX.Element => {
+  return <fieldset className="mb-3">
+    {children}
+  </fieldset>
 }
 
-interface ExpandableQuestionFormFieldProps {
-  question: ExpandableQuestion<ID, ErrorType>;
-  id: string;
+const FormFieldLabel = ({id, prompt}): JSX.Element => {
+  const orderedLabel = id.replaceAll('_', '.')
+
+  return <label htmlFor={id} className='form-label'>{orderedLabel}. {prompt}</label>
 }
 
-const FormField = ({ children }): JSX.Element => {
-  return <fieldset className="mb-3">{children}</fieldset>;
-};
+const PlaceholderFormField = ({question}: {question: QuestionNode<ID, ErrorType>}): JSX.Element => {
+  return <FormField>
+    <FormFieldLabel id={question.getId()} prompt={question.getPrompt()} />
+    <p>(WIP) Non-supported question type</p>
+  </FormField>
+}
 
-const FormFieldLabel = ({ orderedLabelId, id, prompt }): JSX.Element => {
-  const orderedLabel = orderedLabelId.replaceAll('_', '.');
+const NumericQuestionFormField = ({question}: {question: NumericQuestion<ID, ErrorType>}): JSX.Element => {
+  return <FormField>
+    <FormFieldLabel id={question.getId()} prompt={question.getPrompt()} />
+    <input
+      className="form-control w-fit"
+      type="number"
+      min="0"
+      defaultValue={question.getAnswer()}
+    />
+  </FormField>
+}
 
-  return (
-    <label htmlFor={id} className="form-label">
-      {orderedLabel}. {prompt}
-    </label>
-  );
-};
+const TextQuestionFormField = ({question}: {question: TextQuestion<ID, ErrorType>}): JSX.Element => {
 
-const PlaceholderFormField = ({ question, id }: FormFieldProps): JSX.Element => {
-  return (
-    <FormField>
-      <FormFieldLabel orderedLabelId={question.getId()} id={id} prompt={question.getPrompt()} />
-      <p>(WIP) Non-supported question type</p>
-    </FormField>
-  );
-};
+  return <FormField>
+    <FormFieldLabel id={question.getId()} prompt={question.getPrompt()} />
+    <input
+      className="form-control w-fit"
+      type="text"
+      defaultValue={question.getAnswer()}
+    />
+  </FormField>
+}
 
-const NumericQuestionFormField = ({ question, id }: FormFieldProps): JSX.Element => {
-  return (
-    <FormField>
-      <FormFieldLabel orderedLabelId={question.getId()} id={id} prompt={question.getPrompt()} />
-      <input
-        id={id}
-        className="form-control w-fit"
-        type="number"
-        min="0"
-        defaultValue={question.getAnswer()}
-      />
-    </FormField>
-  );
-};
+const ExpandableQuestionFormField = ({question} : {question: ExpandableQuestion<ID, ErrorType>}): JSX.Element => {
+  const [elements, setElements] = useState([])
 
-const TextQuestionFormField = ({ question, id }: FormFieldProps): JSX.Element => {
-  return (
-    <FormField>
-      <FormFieldLabel orderedLabelId={question.getId()} id={id} prompt={question.getPrompt()} />
-      <input className="form-control w-fit" type="text" defaultValue={question.getAnswer()} />
-    </FormField>
-  );
-};
-
-const ExpandableQuestionFormField = ({
-  question,
-  id,
-}: ExpandableQuestionFormFieldProps): JSX.Element => {
-  const [elements, setElements] = useState([]);
-
-  return (
-    <FormField>
-      <FormFieldLabel orderedLabelId={question.getId()} id={id} prompt={question.getPrompt()} />
-      <input
-        className="col-sm form-control w-fit"
-        type="number"
-        min="0"
-        defaultValue={0}
-        onChange={(e) => {
-          question.setAnswer(parseInt(e.target.value));
-          let index = 1;
-          setElements(
-            question.map<JSX.Element>((questions) => {
-              return (
-                <fieldset key={index} className="mt-3">
-                  <h6 className="uppercase text-lg">Item {index++}</h6>
-                  {buildQuestionFormField(questions)}
-                </fieldset>
-              );
-            }),
-          );
-        }}
-      />
-      <div>{elements}</div>
-    </FormField>
-  );
-};
-
-const SingleSelectionQuestionFormField = ({ question, id }: FormFieldProps) => {
-  const [currentSelection, setCurrentSelection] = useState(question.getAnswer() || 0);
-
-  return (
-    <FormField>
-      <FormFieldLabel orderedLabelId={question.getId()} id={id} prompt={question.getPrompt()} />
-      {question.getChoices().map((choice: ImmutableChoice, index) => {
-        return (
-          <div key={`${id}-${index}`}>
-            <input
-              id={`${id}-${index}`}
-              className="form-check-input"
-              name={`${question.getPrompt()}-${id}}`}
-              type="radio"
-              checked={choice.wasChosen()}
-              onChange={() => {
-                question.setAnswer(index);
-              }}
-            />
-            &nbsp;<label htmlFor={`${id}-${index}`}>{choice.getDescription()}</label>
-          </div>
+  return <FormField>
+    <FormFieldLabel id={question.getId()} prompt={question.getPrompt()} />
+    <input className='col-sm form-control w-fit' type='number' min="0" defaultValue={0} onChange={(e) => {
+      question.setAnswer(parseInt(e.target.value));
+      let index = 1;
+      setElements(question.map<JSX.Element>((questions) => {
+          return (
+          <fieldset key={index} className='mt-3'>
+            <h6 className='uppercase text-lg'>Item {index++}</h6>
+            {buildQuestionFormField(questions)}
+          </fieldset>
         );
-      })}
-    </FormField>
-  );
-};
+      }));
+    }} />
+    <div>
+      { elements }
+    </div>
+  </FormField>
+}
 
-const MultiSelectionQuestionFormField = ({ question, id }: FormFieldProps) => {
-  return (
-    <FormField>
-      <FormFieldLabel orderedLabelId={question.getId()} id={id} prompt={question.getPrompt()} />
-      {question.getChoices().map((choice: ImmutableChoice, index) => (
-        <div key={`${id}-${index}`}>
-          <input
-            id={`${id}-${index}`}
-            className="form-check-input"
-            name={`${question.getPrompt()}-${id}}`}
-            type="checkbox"
-            checked={choice.wasChosen()}
-            onChange={() => {
-              question.setAnswer(
-                choice.wasChosen()
-                  ? question.getAnswer().filter((choiceIndex) => choiceIndex !== index)
-                  : question.getAnswer().concat(index),
-              );
-            }}
-          />
-          &nbsp;<label htmlFor={`${id}-${index}`}>{choice.getDescription()}</label>
-        </div>
-      ))}
-    </FormField>
-  );
-};
+const SingleSelectionQuestionFormField = ({question}: {question: SingleSelectionQuestion<ID, ErrorType>}) => {
+  const [currentSelection, setCurrentSelection] = useState(question.getAnswer() || 0)
+
+  return <FormField>
+    <FormFieldLabel id={question.getId()} prompt={question.getPrompt()} />
+    {question.getChoices().map((choice: ImmutableChoice, index) => {
+      return <div key={`${question.getId()}-${index}`}>
+        <input 
+          id={`${question.getId()}-${index}`} 
+          className='form-check-input' 
+          name={question.getPrompt()} 
+          type="radio" 
+          checked={choice.wasChosen()} 
+          onChange={() => {question.setAnswer(index);}
+          } 
+        /> 
+        &nbsp;<label htmlFor={`${question.getId()}-${index}`}>{choice.getDescription()}</label>
+      </div>
+    })}
+  </FormField>
+}
+
+const MultiSelectionQuestionFormField = ({question}: {question: MultipleSelectionQuestion<ID, ErrorType>}) => {
+  return <FormField>
+    <FormFieldLabel id={question.getId()} prompt={question.getPrompt()} />
+    {question.getChoices().map((choice: ImmutableChoice, index) =>
+      <div key={`${question.getId()}-${index}`}>
+        <input 
+          id={`${question.getId()}-${index}`} 
+          className='form-check-input' 
+          name={question.getPrompt()} 
+          type="checkbox" 
+          checked={choice.wasChosen()} 
+          onChange={() => {
+            question.setAnswer(choice.wasChosen() ?
+              question.getAnswer().filter((choiceIndex) => choiceIndex !== index) :
+              question.getAnswer().concat(index) 
+            );
+          }} 
+        /> 
+        &nbsp;<label htmlFor={`${question.getId()}-${index}`}>{choice.getDescription()}</label>
+      </div>
+    )}
+  </FormField>
+}
 
 export const buildQuestionFormField = (questions: QuestionGroup<ID, ErrorType>): JSX.Element => {
-  return (
-    <FormField>
-      {questions
-        .map<[QuestionNode<ID, ErrorType>, FunctionalComponent]>({
-          textQuestion: (q) => [q, TextQuestionFormField],
-          numericQuestion: (q) => [q, NumericQuestionFormField],
-          singleSelectionQuestion: (q) => [q, SingleSelectionQuestionFormField],
-          multipleSelectionQuestion: (q) => [q, MultiSelectionQuestionFormField],
-          questionGroup: (q) => [q, buildQuestionFormField],
-          compositionQuestion: (q) => [q, PlaceholderFormField],
-          expandableQuestion: (q) => [q, ExpandableQuestionFormField],
-        })
-        .map((tuple) => {
-          const question: QuestionNode<ID, ErrorType> = tuple[0];
-          const FormFieldComponent: any = tuple[1];
-          const id = uuid();
+  return <FormField> {questions.map<[QuestionNode<ID, ErrorType>, FunctionalComponent]>({
+    textQuestion: (q) => [q, TextQuestionFormField],
+    numericQuestion: (q) => [q, NumericQuestionFormField],
+    singleSelectionQuestion: (q) => [q, SingleSelectionQuestionFormField],
+    multipleSelectionQuestion: (q) => [q, MultiSelectionQuestionFormField],
+    questionGroup: (q) => [q, buildQuestionFormField],
+    compositionQuestion: (q) => [q, PlaceholderFormField],
+    expandableQuestion: (q) => [q, ExpandableQuestionFormField],
+  }).map((tuple) => {
+    const question: QuestionNode<ID, ErrorType> = tuple[0];
+    const FormFieldComponent: any = tuple[1];
+    
+    return <FormFieldComponent key={question.getId()} question={question} />
+  })} </FormField>;
+}
 
-          return <FormFieldComponent key={id} question={question} id={id} />;
-        })}
-    </FormField>
-  );
-};
