3284dd24bcb18a67ce68b55a11aa4ef1dfe9db18
j <zsoe@d207-023-202-111.wireless.sfu.ca>
2022-12-06 19:13:17 -0800

    move rotate inside function

--- server/src/logger/prod.logger.ts
+++ server/src/logger/prod.logger.ts
@@ -3,35 +3,34 @@ import DailyRotateFile from 'winston-daily-rotate-file';
 
 const { combine, timestamp, errors, json } = format;
 
-const infoTransport = new DailyRotateFile({
-  filename: `logs/hha-info-%DATE%.log`,
-  datePattern: 'YYYY-MM-DD',
-  zippedArchive: true,
-  maxSize: '10m', // 10MB
-  maxFiles: '3d', // keep for 3 days
-  level: 'info'
-});
-
-const errorTransport = new DailyRotateFile({
-  filename: `logs/hha-errors-%DATE%.log`,
-  datePattern: 'YYYY-MM-DD',
-  zippedArchive: true,
-  maxSize: '10m', // 10MB
-  maxFiles: '3d', // keep for 3 days
-  level: 'error'
-});
-
-infoTransport.on('rotate', function (oldFilename, newFilename) {
-  // can upload files somewhere
-});
-
-errorTransport.on('rotate', function (oldFilename, newFilename) {
-  // can upload files somewhere
-});
-
 export const buildProdLogger = (): Logger => {
   // NOTE: this only stores logs in files, and does not log to console
   // we might want to consider loggging to cloudwatch or mongodb or something
+  const infoTransport = new DailyRotateFile({
+    filename: `logs/hha-info-%DATE%.log`,
+    datePattern: 'YYYY-MM-DD',
+    zippedArchive: true,
+    maxSize: '10m', // 10MB
+    maxFiles: '3d', // keep for 3 days
+    level: 'info'
+  });
+
+  const errorTransport = new DailyRotateFile({
+    filename: `logs/hha-errors-%DATE%.log`,
+    datePattern: 'YYYY-MM-DD',
+    zippedArchive: true,
+    maxSize: '10m', // 10MB
+    maxFiles: '3d', // keep for 3 days
+    level: 'error'
+  });
+
+  infoTransport.on('rotate', function (oldFilename, newFilename) {
+    // can upload files somewhere
+  });
+
+  errorTransport.on('rotate', function (oldFilename, newFilename) {
+    // can upload files somewhere
+  });
 
   return createLogger({
     level: 'info',
