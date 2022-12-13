61c466c525a7892e29ad557150dd4575811dfbf7
j <zsoe@Zwes-MacBook-Pro.local>
2022-12-13 13:32:42 -0800

    update readme

--- server/src/logger/index.ts
+++ server/src/logger/index.ts
@@ -2,18 +2,6 @@ import { Logger } from 'winston';
 import { buildDevLogger } from './dev.logger';
 import { buildProdLogger } from './prod.logger';
 
-/**
-    levels {
-        error: 0,
-        warn: 1,
-        info: 2,
-        http: 3,
-        verbose: 4,
-        debug: 5,
-        silly: 6
-    }   
-**/
-
 let logger: Logger;
 
 if (process.env.NODE_ENV !== 'production') {
