4e8e7f76d2b7ac228736940e380281021f413473
David Doan <vdoan@sfu.ca>
2023-01-26 16:33:56 -0800

    Fix useDepartmentMap hook and refactor edit

--- client/src/pages/admin/edit/edit.tsx
+++ client/src/pages/admin/edit/edit.tsx
@@ -1,12 +1,10 @@
 import { useState, useEffect, useMemo } from 'react';
 import { Link, useHistory, useParams } from 'react-router-dom';
-import { Department, UserJson } from 'constants/interfaces';
+import { UserJson } from 'constants/interfaces';
 import { AdminUserFormData } from 'pages/admin/typing';
 import SideBar from 'components/side_bar/side_bar';
 import Header from 'components/header/header';
 import Api from 'actions/Api';
-import { ENDPOINT_DEPARTMENT_GET } from 'constants/endpoints';
-import { createDepartmentMap } from 'utils/departmentMapper';
 import { ENDPOINT_ADMIN_GET_BY_ID, ENDPOINT_ADMIN_PUT_BY_ID } from 'constants/endpoints';
 import { useTranslation } from 'react-i18next';
 import { History } from 'history';
@@ -16,56 +14,38 @@ import useDidMountEffect from 'utils/custom_hooks';
 import { AdminUserForm } from 'pages/admin/form/form';
 import { Paths, UserIdParams } from 'constants/paths';
 import { ResponseMessage } from 'utils/response_message';
+import { useDepartmentMap } from 'hooks';
 
 import './edit.css';
 
 interface UserEditProps {}
 
 export const EditUserForm = (props: UserEditProps) => {
-  const [departments, setDepartments] = useState<Map<string, Department>>(undefined);
+  const departments = useDepartmentMap();
   const [fetch, setFetch] = useState<boolean>(false);
   const [user, setUser] = useState<UserJson>(undefined);
   const params = useParams<UserIdParams>();
   const id = useMemo<string>(() => params.userId, [params.userId]);
-
   const history: History = useHistory<History>();
   const { t } = useTranslation();
 
-  useEffect(
-    function fetchInitData() {
-      let isMounted = true;
-      const fetchAndSetUser = async () => {
-        const fetchedUser: UserJson = await Api.Get(
-          ENDPOINT_ADMIN_GET_BY_ID(id),
-          ResponseMessage.getMsgFetchUserFailed(),
-          history,
-        );
-        if (isMounted) setUser(fetchedUser);
-      };
-      fetchAndSetUser();
-
-      const fetchAndSetDepartments = async () => {
-        const response: Department[] = await Api.Get(
-          ENDPOINT_DEPARTMENT_GET,
-          ResponseMessage.getMsgFetchDepartmentsFailed(),
-          history,
-        );
-        if (isMounted) setDepartments(createDepartmentMap(response));
-      };
-      fetchAndSetDepartments();
-
-      return function cleanUp() {
-        isMounted = false;
-      };
-    },
-    [history, id],
-  );
+  useEffect(() => {
+    const fetchAndSetUser = async () => {
+      const fetchedUser: UserJson = await Api.Get(
+        ENDPOINT_ADMIN_GET_BY_ID(id),
+        ResponseMessage.getMsgFetchUserFailed(),
+        history,
+      );
+      setUser(fetchedUser);
+    };
+    fetchAndSetUser();
+  }, [history, id]);
 
   useDidMountEffect(
     function signalInitDataReady() {
-      if (user !== undefined && departments !== undefined) setFetch(true);
+      if (user !== undefined || !departments) setFetch(true);
     },
-    [user, departments],
+    [user],
   );
 
   const onSubmit = () => {
@@ -86,7 +66,7 @@ export const EditUserForm = (props: UserEditProps) => {
   return (
     <div className={'admin'}>
       <SideBar />
-      {fetch === false ? (
+      {!fetch ? (
         <Spinner></Spinner>
       ) : (
         <main className="container-fluid main-region">
