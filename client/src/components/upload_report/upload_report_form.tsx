a2c7c0f5a8b2f676de97e01102fa12a323b29cab
Peeyush <peeyush_sharma@vancity.com>
2023-03-20 14:20:05 -0700

    Json upload of report templates configured

--- client/src/components/upload_report/upload_report_form.tsx
+++ client/src/components/upload_report/upload_report_form.tsx
@@ -1,8 +1,36 @@
-import React, { useState } from 'react';
+import React, { useState, Dispatch, SetStateAction } from 'react';
+import { ObjectSerializer, QuestionGroup } from '@hha/common';
 
-export const UploadForm = (): JSX.Element => {
-  const [formData, setFormData] = useState(null);
+import Api from 'actions/Api';
+
+export const UploadForm = ({
+  applyReportChanges,
+  formHandler,
+  isSubmitting,
+  reportTemplateData,
+  updateReport,
+}: {
+  applyReportChanges: () => void;
+  formHandler: (event: React.FormEvent<HTMLFormElement>) => void;
+  isSubmitting: boolean;
+  reportTemplateData: QuestionGroup<ID, ErrorType>;
+  updateReport: Dispatch<SetStateAction<QuestionGroup<string, string>>>;
+}): JSX.Element => {
   const [error, setError] = useState(null);
+  const objectSerializer: ObjectSerializer = ObjectSerializer.getObjectSerializer();
+
+  const buildSubmitButton = () => {
+    return (
+      <>
+        <input
+          className="btn btn-outline-primary"
+          disabled={!reportTemplateData || isSubmitting}
+          type="submit"
+          value="Submit Report Template"
+        />
+      </>
+    );
+  };
 
   const handleFileChange = (event) => {
     const file = event.target.files[0];
@@ -11,10 +39,10 @@ export const UploadForm = (): JSX.Element => {
     reader.onload = (event) => {
       try {
         const data = JSON.parse(String(event.target.result));
-        setFormData(data);
+        updateReport(objectSerializer.deserialize(data.reportObject));
         setError(null);
       } catch (e) {
-        setFormData(null);
+        updateReport(null);
         setError('Error parsing JSON data. Please upload a valid JSON file.');
       }
     };
@@ -26,23 +54,25 @@ export const UploadForm = (): JSX.Element => {
   };
 
   return (
-    <div>
-    
+    <div className="mt-3 p-3">
+      <form onSubmit={formHandler} noValidate>
+        <br />
 
-      <br />
+        <label htmlFor="file">Upload JSON Form:</label>
+        <input id="file" type="file" accept=".json" onChange={handleFileChange} />
 
-      <label htmlFor="file">Upload JSON Form:</label>
-      <input id="file" type="file" accept=".json" onChange={handleFileChange} />
+        {error && <p style={{ color: 'red' }}>{error}</p>}
 
-      <br />
-
-      {error && <p style={{ color: 'red' }}>{error}</p>}
-
-      {formData && (
-        <div>
-          <pre>{JSON.stringify(formData, null, 2)}</pre>
-        </div>
-      )}
+        {buildSubmitButton()}
+        {reportTemplateData && (
+          <>
+            <div>
+              <pre>{JSON.stringify(reportTemplateData, null, 2)}</pre>
+            </div>
+            {buildSubmitButton()}
+          </>
+        )}
+      </form>
     </div>
   );
 };
--- client/src/pages/upload_report/upload_report.tsx
+++ client/src/pages/upload_report/upload_report.tsx
@@ -19,19 +19,19 @@ export const UploadReport = () => {
   const [currentDepartment, setCurrentDepartment] = useState<Department>();
   const [isShowingModal, setIsShowingModal] = useState(false);
   const [isSubmitting, setIsSubmitting] = useState(false);
-  const [report, setReport] = useState<QuestionGroup<ID, ErrorType>>();
+  const [reportTemplate, setReportTemplate] = useState<QuestionGroup<ID, ErrorType>>();
   const history: History = useHistory<History>();
   const objectSerializer: ObjectSerializer = ObjectSerializer.getObjectSerializer();
   const user = useAuthState();
   const { departments } = useDepartmentData();
 
   const applyReportChanges = () => {
-    setReport(objectSerializer.deserialize(objectSerializer.serialize(report)));
+    setReportTemplate(objectSerializer.deserialize(objectSerializer.serialize(reportTemplate)));
   };
 
   const clearCurrentDepartment = (): void => {
     setCurrentDepartment(undefined);
-    setReport(undefined);
+    setReportTemplate(undefined);
   };
 
   const confirmSubmission = (event: React.FormEvent<HTMLFormElement>) => {
@@ -40,24 +40,18 @@ export const UploadReport = () => {
   };
 
   const submitReport = () => {
-    const today = new Date();
-    const serializedReport = objectSerializer.serialize(report);
-    const reportPrompt = serializedReport['prompt'];
-    serializedReport['id'] = generateFormId(user?.userDetails?.name, reportPrompt);
+    const serializedReport = objectSerializer.serialize(reportTemplate);
     const reportObject = {
       departmentId: currentDepartment.id,
-      reportMonth: new Date(today.getFullYear(), today.getMonth()),
       serializedReport,
-      submittedUserId: user?.userDetails?.id,
-      submittedBy: user?.userDetails?.name,
     };
 
     setIsShowingModal(false);
     setIsSubmitting(true);
     Api.Post(
-      ENDPOINT_REPORTS,
+      ENDPOINT_TEMPLATE,
       reportObject,
-      () => history.push(`/department/${currentDepartment.id}`),
+      () => history.push(`/home`),
       history,
       ERR_TOAST,
       PENDING_TOAST,
@@ -93,31 +87,6 @@ export const UploadReport = () => {
     const second = now.getSeconds().toString().padStart(2, '0');
     return `${reportPrompt}_${year}-${month}-${day}_${hour}-${minute}-${second}-${userId}`;
   }
-  useEffect(() => {
-    const controller = new AbortController();
-    const getTemplates = async () => {
-      try {
-        const fetchedTemplateObject = await Api.Get(
-          `${ENDPOINT_TEMPLATE}/${currentDepartment.id}`,
-          '',
-          history,
-          controller.signal,
-        );
-        const reportTemplateJson = fetchedTemplateObject.template.reportObject;
-
-        const deserializedReportTemplate: QuestionGroup<ID, ErrorType> =
-          objectSerializer.deserialize(reportTemplateJson);
-
-        setReport(deserializedReportTemplate);
-      } catch (e) {
-        clearCurrentDepartment();
-      }
-    };
-    currentDepartment && getTemplates();
-    return () => {
-      controller.abort();
-    };
-  }, [currentDepartment, history, objectSerializer]);
 
   return (
     <div className="department">
@@ -128,7 +97,7 @@ export const UploadReport = () => {
           messages={[
             <>
               Please click <strong>Confirm</strong> to proceed with your submission. You'll be
-              redirected to the main {currentDepartment?.name} view.
+              redirected to the home page view.
             </>,
             <>
               If you've made a mistake, please click <strong>Cancel</strong> instead.
@@ -139,11 +108,13 @@ export const UploadReport = () => {
           show={isShowingModal}
           title={'Confirm Submission'}
         />
-        {!report && departments && (
+        {departments && (
           <div className="col-md-6 mb-5">
             <h1 className="text-start">Submit a report template</h1>
             <fieldset>
-              <label htmlFor="">Please select the Department that you are uploading a report template for</label>
+              <label htmlFor="">
+                Please select the Department that you are uploading a report template for
+              </label>
               <select
                 className="form-control"
                 id="Report-Department-Type"
@@ -160,7 +131,15 @@ export const UploadReport = () => {
                     </option>
                   ))}
               </select>
-              <UploadForm/>
+              {currentDepartment && (
+                <UploadForm
+                  applyReportChanges={applyReportChanges}
+                  formHandler={confirmSubmission}
+                  isSubmitting={isSubmitting}
+                  reportTemplateData={reportTemplate}
+                  updateReport={setReportTemplate}
+                />
+              )}
             </fieldset>
           </div>
         )}
