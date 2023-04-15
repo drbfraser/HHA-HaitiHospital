ffb5e438eb75540776560994c3db9389ae3b4bfc
Alec Wang <alec.sy.wang@gmail.com>
2023-04-15 14:42:11 -0700

    ran format

--- client/src/tests/unitTests/app.spec.tsx
+++ client/src/tests/unitTests/app.spec.tsx
@@ -1,11 +1,11 @@
-import React from "react";
-import { shallow } from "enzyme";
-import chai from "chai";
-import chaiEnzyme from "chai-enzyme";
+import React from 'react';
+import { shallow } from 'enzyme';
+import chai from 'chai';
+import chaiEnzyme from 'chai-enzyme';
 chai.use(chaiEnzyme());
 
-describe("App", () => {
-  it("always passes tests", () => {
+describe('App', () => {
+  it('always passes tests', () => {
     expect(true).to.equal(true);
   });
 });
