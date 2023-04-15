e468c5ba064a1031e3b05fd4c14e6512cddbbcaa
Alec Wang <alec.sy.wang@gmail.com>
2023-04-15 14:37:58 -0700

    added test file example

---
+++ client/src/tests/unitTests/app.spec.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { shallow } from "enzyme";
+import chai from "chai";
+import chaiEnzyme from "chai-enzyme";
+chai.use(chaiEnzyme());
+
+describe("App", () => {
+});
