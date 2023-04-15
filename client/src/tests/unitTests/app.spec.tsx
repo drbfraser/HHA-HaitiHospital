9cbb3db9a81c0f194f4ad1b518f3b08b2a7a7c8c
Alec Wang <alec.sy.wang@gmail.com>
2023-04-15 14:53:21 -0700

    added expect import

--- client/src/tests/unitTests/app.spec.tsx
+++ client/src/tests/unitTests/app.spec.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+
 import chai from 'chai';
+import { expect } from 'chai';
 import chaiEnzyme from 'chai-enzyme';
 chai.use(chaiEnzyme());
 
