1ba603bc0141899a5ccb26265e31c473e009cf99
yla614 <leeyoojin210@gmail.com>
2023-06-05 21:18:36 -0700

    Revert "Run prettier"
    This reverts commit d4126fec8bb5e04ae9dcbc29bc64b5deb488eab6.

--- server/src/utils/departments.ts
+++ server/src/utils/departments.ts
@@ -9,7 +9,6 @@ export enum DefaultDepartments {
   Maternity = 'Maternity',
   Community = 'Community & Health',
   BioMechanic = 'Bio Support',
-  Report = 'Report',
 }
 
 // ***************************************************** Utility functions for hashtable approach *****************************************************
