685f70d3aa274c8f11e169e88e87df79011ceb3e
yla614 <leeyoojin210@gmail.com>
2023-07-11 00:32:00 -0700

    Ran prettier

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,22 +1,22 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/index.ts',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js',
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(ts|tsx)$/,
-                //exclude: /node_modules/,
-                use: 'ts-loader',
-            },
-            // Other rules...
-        ],
-    },
-    resolve: {
-        extensions: ['.ts', '.tsx', '.js'],
-    },
+  entry: './src/index.ts',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        //exclude: /node_modules/,
+        use: 'ts-loader',
+      },
+      // Other rules...
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+  },
 };
