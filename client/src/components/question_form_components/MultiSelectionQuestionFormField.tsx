17040902d1161768efd8074c7c0ba3ba7b3a2933
Sagardeep Singh Hunjan <sagardeep_singh_hunjan@sfu.ca>
2023-07-13 19:03:41 -0700

    Fixed formatting

--- client/src/components/question_form_components/MultiSelectionQuestionFormField.tsx
+++ client/src/components/question_form_components/MultiSelectionQuestionFormField.tsx
@@ -56,10 +56,7 @@ const MultiSelectionQuestionFormField = ({
   }, []);
 
   return (
-    <FormFieldCheck
-      nameId={nameId}
-      prompt={question.getPrompt()}
-    >
+    <FormFieldCheck nameId={nameId} prompt={question.getPrompt()}>
       <div>
         {inputState !== true && <div className="text-danger">{inputState.message}</div>}
         {question.getChoices().map((choice: ImmutableChoice, index) => (
