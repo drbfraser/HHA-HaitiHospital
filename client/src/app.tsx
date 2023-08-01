ed664b2f131429ccf715bee680aa3ed1381c7276
Sagardeep Singh Hunjan <sagardeep_singh_hunjan@sfu.ca>
2023-07-31 18:09:15 -0700

    Fixed report crash without language selection

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -1,33 +1,14 @@
-import 'react-toastify/dist/ReactToastify.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'react-toastify/dist/ReactToastify.css';
 
 import { AdminToggleProvider, AuthProvider } from 'contexts';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { BrowserRouter as Router, Switch } from 'react-router-dom';
-import { getWebInstrumentations, initializeFaro } from '@grafana/faro-web-sdk';
 
 import AppRoute from './app-route';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { ToastContainer } from 'react-toastify';
 import routes from './routes';
-
-// Add the following code snippet to your application before any other JavaScript/TypeScript code!
-// For example put the code in your root index.[ts|js] file, right before you initialize your SPA / App.
-
-process.env.REACT_APP_FARO_URL &&
-  initializeFaro({
-    url:
-      process.env.REACT_APP_FARO_URL,
-    app: {
-      name: process.env.REACT_APP_FARO_APP_NAME || 'HHA Web',
-      version: process.env.REACT_APP_FARO_APP_VERSION || '0.0.0',
-      environment: process.env.REACT_APP_FARO_APP_ENVIRONMENT || 'development',
-    },
-    instrumentations: [
-      // Mandatory, overwriting the instrumentations array would cause the default instrumentations to be omitted
-      ...getWebInstrumentations(),
-    ],
-  });
 
 const queryClient = new QueryClient();
 
