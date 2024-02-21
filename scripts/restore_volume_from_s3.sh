e33c7174b539d85c909322b52593ff2cc5de68d5
cta106 <cta106@sfu.ca>
2024-02-20 17:50:08 -0800

    Update restore script to use MongoDB instead of Postgres

--- scripts/restore_volume_from_s3.sh
+++ scripts/restore_volume_from_s3.sh
@@ -2,7 +2,7 @@
 
 # Download a previous backup archive from S3.
 # It will not overwrite any existing data: it just downloads into the root (~/) folder.
-# You must then manually extract and apply the saved data to the Postgres volume
+# You must then manually extract and apply the saved data to the MongoDB volume
 # (see directions below).
 
 # Change to working directory of file not link
@@ -17,11 +17,10 @@ if [ ! -f "../.env" ]; then
 fi
 
 # Needed from .env
-# POSTGRES_USER
 # S3_BUCKET_NAME
 source "$SCRIPT_DIR/../.env"
 
-DB_NAME=cbr
+DB_NAME=haiti
 
 # Ask which folder
 echo "Choose which backup type (folder) you want to restore from."
@@ -50,33 +49,29 @@ echo "# To restore data to active system:"
 echo "# 1. Extract downloaded file"
 echo "       mkdir ~/restore/; cd ~/restore/"
 echo "       tar -xzvf ~/$selected_backup"
-echo "# 2. Stop running containers; but re-run postgres in background:"
-echo "       cd ~/cbr"
+echo "# 2. Stop running containers; but re-run mongodb in background:"
+echo "       cd ~/haiti"
 echo "       source ./scripts/set-image-tag.sh"
 echo "       docker compose -f docker-compose.yml -f docker-compose.deploy.yml down"
-echo "       docker compose -f docker-compose.yml -f docker-compose.deploy.yml up -d postgres"
+echo "       docker compose -f docker-compose.yml -f docker-compose.deploy.yml up -d mongodb"
 echo "# 3. Save a copy of the current docker volume just in case (ONE OF THESE):"
 echo "#    If using block storage, then:"
-echo "#      cp -r /mnt/blockstorage/165536.165536/volumes/*_cbr_postgres_data/ ~/deleteme_cbr_postgres_volume_copy"
+echo "#      cp -r /mnt/blockstorage/165536.165536/volumes/*_hhahaiti_mongodb_data/ ~/deleteme_haiti_mongodb_volume_copy"
 echo "#    Otherwise:"
-echo "#      cp -r /var/lib/docker/165536.165536/volumes/*_cbr_postgres_data/ ~/deleteme_cbr_postgres_volume_copy"
-echo "# 4. If DB being restored and current DB have different POSTGRES_USER, then:"
-echo "#    Backup POSTGRES_USER: "
-echo "        cat ~/restore/backup.env | grep POSTGRES_USER"
-echo "#    Current POSTGRES_USER=$POSTGRES_USER"
-echo "#       docker exec cbr_postgres    psql -U $POSTGRES_USER template1 -c \"CREATE USER <old-POSTGRES_USER> WITH CREATEDB PASSWORD '<old-POSTGRES_PASSWORD>';\" "
-echo "# 5. Wipe the DB and apply the extracted SQL file to the Postgres volume:"
-echo "        docker exec cbr_postgres    psql -U $POSTGRES_USER template1 -c 'drop database $DB_NAME;'"
-echo "        docker exec cbr_postgres    psql -U $POSTGRES_USER template1 -c 'create database $DB_NAME with owner $POSTGRES_USER;'"
-echo "        docker cp ~/restore/cbr_db_backup_*.sql cbr_postgres:/restore.sql"
-echo "        docker exec cbr_postgres    psql -U $POSTGRES_USER $DB_NAME -f /restore.sql"
-echo "# 6. Restart all the containers:"
+echo "#      cp -r /var/lib/docker/165536.165536/volumes/*_hhahaiti_mongodb_data/ ~/deleteme_haiti_mongodb_volume_copy"
+echo "# 4. Restore the BSON file to the MongoDB volume:"
+echo "        docker cp ~/restore/haiti_db_backup_*.bson hhahaiti_mongodb:/restore.bson"
+echo "        docker exec hhahaiti_mongodb mongorestore --drop --archive=/restore.bson"
+echo "# 5. Restart all the containers:"
 echo "        docker compose -f docker-compose.yml -f docker-compose.deploy.yml down"
 echo "        docker compose -f docker-compose.yml -f docker-compose.deploy.yml up -d"
-echo "# 7. Copy /uploads/ directory from extracted backup to the cbr_django container (may report 0B copied):"
-echo "        docker cp - cbr_django:/  <  ~/restore/uploads.tar"
-echo "# 8. Run data migration:"
-echo "        docker exec cbr_django python manage.py migrate"
+
+# TODO: This is for python django, not sure if our tech stack need something like this or not
+# echo "# 6. Copy /uploads/ directory from extracted backup to the haiti_django container (may report 0B copied):"
+# echo "        docker cp - haiti_django:/  <  ~/restore/uploads.tar"
+# echo "# 7. Run data migration:"
+# echo "        docker exec haiti_django python manage.py migrate"
+
 echo "#"
 echo "# Later, after the server is working for a while, cleanup:"
-echo "#       rm -rf ~/deleteme_cbr_postgres_volume_copy"
+echo "#       rm -rf ~/deleteme_haiti_mongodb_volume_copy"
