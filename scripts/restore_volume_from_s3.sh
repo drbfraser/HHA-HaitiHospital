f0bc91c9f6e7bab0363394742897c834b1bde0fa
cta106 <cta106@sfu.ca>
2024-02-21 21:53:29 -0800

    added reseed method and made the guide more user friendly

--- scripts/restore_volume_from_s3.sh
+++ scripts/restore_volume_from_s3.sh
@@ -43,35 +43,40 @@ read -p "Enter the name of the backup you want to restore: " selected_backup
 aws s3 cp "s3://$S3_BUCKET_NAME/$FOLDER/$selected_backup" ~/
 
 # Show directions for what to do next
-echo "Backup tar file successfully downloaded to: ~/$selected_backup"
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+BLUE='\033[0;34m'
+NC='\033[0m' # No Color
+
+echo -e "${GREEN}Backup tar file successfully downloaded to: ~/$selected_backup${NC}"
 echo
-echo "# To restore data to active system:"
-echo "# 1. Extract downloaded file"
+echo -e "${BLUE}To restore data to active system:${NC}"
+echo -e "${RED}1. Extract downloaded file${NC}"
 echo "       mkdir ~/restore/; cd ~/restore/"
 echo "       tar -xzvf ~/$selected_backup"
-echo "# 2. Stop running containers; but re-run mongodb in background:"
+echo
+echo -e "${RED}2. Stop running containers; but re-run mongodb in background:${NC}"
 echo "       cd ~/haiti"
 echo "       source ./scripts/set-image-tag.sh"
 echo "       docker compose -f docker-compose.yml -f docker-compose.deploy.yml down"
 echo "       docker compose -f docker-compose.yml -f docker-compose.deploy.yml up -d mongodb"
-echo "# 3. Save a copy of the current docker volume just in case (ONE OF THESE):"
-echo "#    If using block storage, then:"
-echo "#      cp -r /mnt/blockstorage/165536.165536/volumes/*_hhahaiti_mongodb_data/ ~/deleteme_haiti_mongodb_volume_copy"
-echo "#    Otherwise:"
-echo "#      cp -r /var/lib/docker/165536.165536/volumes/*_hhahaiti_mongodb_data/ ~/deleteme_haiti_mongodb_volume_copy"
-echo "# 4. Restore the BSON file to the MongoDB volume:"
+echo
+echo -e "${RED}3. Save a copy of the current docker volume just in case (ONE OF THESE):${NC}"
+echo "    If using block storage, then:"
+echo "      cp -r /mnt/blockstorage/165536.165536/volumes/*_hhahaiti_mongodb_data/ ~/deleteme_haiti_mongodb_volume_copy"
+echo "    Otherwise:"
+echo "      cp -r /var/lib/docker/165536.165536/volumes/*_hhahaiti_mongodb_data/ ~/deleteme_haiti_mongodb_volume_copy"
+echo
+echo -e "${RED}4. Restore the BSON file to the MongoDB volume:${NC}"
 echo "        docker cp ~/restore/haiti_db_backup_*.bson hhahaiti_mongodb:/restore.bson"
 echo "        docker exec hhahaiti_mongodb mongorestore --drop --archive=/restore.bson"
-echo "# 5. Restart all the containers:"
+echo
+echo -e "${RED}5. Reseed the server or database:${NC}"
+echo "        docker exec -it hhahaiti_server /bin/bash -c 'npm run seed'"
+echo
+echo -e "${RED}6. Restart all the containers:${NC}"
 echo "        docker compose -f docker-compose.yml -f docker-compose.deploy.yml down"
 echo "        docker compose -f docker-compose.yml -f docker-compose.deploy.yml up -d"
-
-# TODO: This is for python django, not sure if our tech stack need something like this or not
-# echo "# 6. Copy /uploads/ directory from extracted backup to the haiti_django container (may report 0B copied):"
-# echo "        docker cp - haiti_django:/  <  ~/restore/uploads.tar"
-# echo "# 7. Run data migration:"
-# echo "        docker exec haiti_django python manage.py migrate"
-
-echo "#"
-echo "# Later, after the server is working for a while, cleanup:"
-echo "#       rm -rf ~/deleteme_haiti_mongodb_volume_copy"
+echo
+echo -e "${BLUE}Later, after the server is working for a while, cleanup:${NC}"
+echo "       rm -rf ~/deleteme_haiti_mongodb_volume_copy"
