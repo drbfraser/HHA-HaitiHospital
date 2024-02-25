dcc4f71850d50e65bbe488979e7189d95d77beac
bra40 <brian_rahadi@sfu.ca>
2024-02-25 10:58:56 -0800

    Init server WIP

--- server/src/routes/api/authentication.ts
+++ server/src/routes/api/authentication.ts
@@ -1,7 +1,6 @@
 import { NextFunction, Request, Response, Router } from 'express';
 
 import { HTTP_OK_CODE } from 'exceptions/httpException';
-import { RequestWithUser } from 'utils/definitions/express';
 import UserCollection from 'models/user';
 import { logger } from '../../logger';
 import requireJwtAuth from 'middleware/requireJwtAuth';
@@ -9,7 +8,7 @@ import requireLocalAuth from '../../middleware/requireLocalAuth';
 
 const router = Router();
 
-router.post('/login', requireLocalAuth, async (req: RequestWithUser, res: Response) => {
+router.post('/login', requireLocalAuth, async (req: Request, res: Response) => {
   const user = req.body;
   const mongooseUser = await UserCollection.findOne({ username: user.username });
   const jsonUser = await mongooseUser!.toJson();
@@ -20,20 +19,16 @@ router.post('/login', requireLocalAuth, async (req: RequestWithUser, res: Respon
     .json({ success: true, isAuth: true, user: jsonUser, csrfToken: req.body._csrf });
 });
 
-router.post(
-  '/logout',
-  requireJwtAuth,
-  (req: RequestWithUser, res: Response, next: NextFunction) => {
-    res.cookie('jwt', 'invalidated-jwt-token');
-    req.logout((err) => {
-      if (err) {
-        return next(err);
-      }
-    });
-    logger.debug('User successfully logged out');
-    res.send(true);
-  },
-);
+router.post('/logout', requireJwtAuth, (req: Request, res: Response, next: NextFunction) => {
+  res.cookie('jwt', 'invalidated-jwt-token');
+  req.logout((err) => {
+    if (err) {
+      return next(err);
+    }
+  });
+  logger.debug('User successfully logged out');
+  res.send(true);
+});
 
 router.get('/csrftoken', (req: Request, res: Response) => {
   res.json({ CSRFToken: req.csrfToken() });
