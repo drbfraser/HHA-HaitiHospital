4ec9b99420a1cf413245e91c8470e4e4c3526210
bra40 <brian_rahadi@sfu.ca>
2024-03-10 13:30:03 -0700

    Move leaderboard types

--- client/src/pages/leader_board/LeaderBoard.tsx
+++ client/src/pages/leader_board/LeaderBoard.tsx
@@ -11,7 +11,7 @@ import { History } from 'history';
 import Layout from 'components/layout';
 import { useHistory } from 'react-router';
 import { useTranslation } from 'react-i18next';
-import { Leaderboard } from './typing';
+import { Leaderboard } from 'constants/interfaces';
 
 export const LeaderBoard = () => {
   const { t } = useTranslation();
