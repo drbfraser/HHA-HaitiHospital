b2cba00231ae8c1491b2e89e93a96f72fc42ddcf
Justin Ang <jrang@sfu.ca>
2024-03-28 01:40:58 -0700

    finished custom month year form

--- client/src/components/form/MonthField.tsx
+++ client/src/components/form/MonthField.tsx
@@ -1,43 +1,58 @@
+import { Dispatch, SetStateAction, useState } from 'react';
 import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
+import { currMonth, currYear } from 'utils';
+import { useTranslation } from 'react-i18next';
 
-export const MonthField = ({}) => (
-  <div className="">
-    <Row>
-      <Col>
-        <Form.Group className="mb-3" controlId="month">
-          <Form.Label htmlFor="month">Month:</Form.Label>
-          <Form.Select id="month" name="month" defaultValue={new Date().getMonth()}>
-            <option value={1}>January</option>
-            <option value={2}>February</option>
-            <option value={3}>March</option>
-            <option value={4}>April</option>
-            <option value={5}>May</option>
-            <option value={6}>June</option>
-            <option value={7}>July</option>
-            <option value={8}>August</option>
-            <option value={9}>September</option>
-            <option value={10}>October</option>
-            <option value={11}>November</option>
-            <option value={12}>December</option>
-          </Form.Select>
-        </Form.Group>
-      </Col>
-      <Col>
-        <Form.Group className="mb-3" controlId="year">
-          <Form.Label htmlFor="year">Year:</Form.Label>
-          <Form.Control
-            type="number"
-            id="year"
-            name="year"
-            min="1900"
-            max={new Date().getFullYear()}
-            defaultValue={new Date().getFullYear()}
-            step="1"
-          />
-        </Form.Group>
-      </Col>
-    </Row>
-  </div>
-);
+interface MonthFieldProps {
+  setReportMonth: Dispatch<SetStateAction<Date | undefined>>;
+  reportMonth: Date | undefined;
+}
+
+export const MonthField = () => {
+  const [month, setMonth] = useState<number>(currMonth);
+  const [year, setYear] = useState<number>(currYear);
+  const [reportMonth, setReportMonth] = useState<Date | undefined>();
+  const { t } = useTranslation();
+
+  return (
+    <div className="">
+      <Row>
+        <Col>
+          <Form.Group className="mb-3" controlId="month">
+            <Form.Label htmlFor="month">{t('month')}</Form.Label>
+            <Form.Select id="month" name="month">
+              <option value={0}>{t('monthJanuary')}</option>
+              <option value={1}>{t('monthFebruary')}</option>
+              <option value={2}>{t('monthMarch')}</option>
+              <option value={3}>{t('monthApril')}</option>
+              <option value={4}>{t('monthMay')}</option>
+              <option value={5}>{t('monthJune')}</option>
+              <option value={6}>{t('monthJuly')}</option>
+              <option value={7}>{t('monthAugust')}</option>
+              <option value={8}>{t('monthSeptember')}</option>
+              <option value={9}>{t('monthOctober')}</option>
+              <option value={10}>{t('monthNovember')}</option>
+              <option value={11}>{t('monthDecember')}</option>
+            </Form.Select>
+          </Form.Group>
+        </Col>
+        <Col>
+          <Form.Group className="mb-3" controlId="year">
+            <Form.Label htmlFor="year">{t('year')}</Form.Label>
+            <Form.Control
+              type="number"
+              id="year"
+              name="year"
+              min="1900"
+              max={year}
+              defaultValue={year}
+              step="1"
+            />
+          </Form.Group>
+        </Col>
+      </Row>
+    </div>
+  );
+};
--- client/src/components/report/ReportAndTemplateForm.tsx
+++ client/src/components/report/ReportAndTemplateForm.tsx
@@ -4,6 +4,7 @@ import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
 import { Department } from 'constants/interfaces';
 import { useTranslation } from 'react-i18next';
+import { MonthField } from 'components/form/MonthField';
 
 interface ReportAndTemplateFormProps {
   departmentLabel: string;
@@ -59,43 +60,7 @@ export const ReportAndTemplateForm = ({
           max={new Date().toISOString().slice(0, 7)}
           required
         />
-        <div className="reactBootstrap mt-2">
-          <Row>
-            <Col>
-              <Form.Group className="mb-3" controlId="month">
-                <Form.Label htmlFor="month">Month:</Form.Label>
-                <Form.Select id="month" name="month" defaultValue={new Date().getMonth()}>
-                  <option value={1}>January</option>
-                  <option value={2}>February</option>
-                  <option value={3}>March</option>
-                  <option value={4}>April</option>
-                  <option value={5}>May</option>
-                  <option value={6}>June</option>
-                  <option value={7}>July</option>
-                  <option value={8}>August</option>
-                  <option value={9}>September</option>
-                  <option value={10}>October</option>
-                  <option value={11}>November</option>
-                  <option value={12}>December</option>
-                </Form.Select>
-              </Form.Group>
-            </Col>
-            <Col>
-              <Form.Group className="mb-3" controlId="year">
-                <Form.Label htmlFor="year">Year:</Form.Label>
-                <Form.Control
-                  type="number"
-                  id="year"
-                  name="year"
-                  min="1900"
-                  max={new Date().getFullYear()}
-                  defaultValue={new Date().getFullYear()}
-                  step="1"
-                />
-              </Form.Group>
-            </Col>
-          </Row>
-        </div>
+        <MonthField />
       </fieldset>
     </div>
   );
