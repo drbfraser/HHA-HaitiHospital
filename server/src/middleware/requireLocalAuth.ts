a7bb72096b22a4e01368f48d56715bfe26448ffb
Filipo Marcellino <filipo_marcellino@sfu.ca>
2024-06-19 18:05:48 -0700

    smarter logs for failed login

--- server/src/middleware/requireLocalAuth.ts
+++ server/src/middleware/requireLocalAuth.ts
@@ -12,7 +12,12 @@ const requireLocalAuth = (req: Request, res: Response, next: NextFunction) => {
       return next(err);
     }
     if (!user) {
-      logger.error('Cannot authenticate request, user does not exist.');
+      logger.error({
+        message: `Failed login attempt, user-password pair does not exist.`,
+        requestUrl: req.originalUrl,
+        requestMethod: req.method,
+        requestBody: req.body,
+      });
       return res.status(HTTP_UNAUTHORIZED_CODE).send(info);
     }
     (req as RequestWithUser).user = user;
