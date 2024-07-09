fa4157ace32627fde7fe9d2b95de91097c02c1e5
Joshua Li <joshua_li@sfu.ca>
2024-07-09 12:16:14 -0700

    feat(client): Add "Next Page" button when filling out a report.
    Issue: https://github.sfu.ca/bfraser/415-HHA-Haiti/issues/709

--- client/src/components/report/ReportForm.tsx
+++ client/src/components/report/ReportForm.tsx
@@ -257,6 +257,16 @@ const ReportForm = ({
               value={t('reportSaveAsDraft')}
             />
           </div>
+          <div className="position-sticky py-3">
+            <input
+              className="btn btn-primary"
+              name="next"
+              type="button"
+              disabled={currentPage == numberOfPages}
+              onClick={() => setCurrentPage(currentPage + 1)}
+              value={t(`reportNextPage`)}
+            />
+          </div>
         </div>
         <div className="text-danger">
           <p>{submitTooltipText}</p>
