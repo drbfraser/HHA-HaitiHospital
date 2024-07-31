769fce8a1c558270c0ccb8d23ab28c83a64ac39b
Filipo Marcellino <filipo_marcellino@sfu.ca>
2024-07-31 10:55:59 -0700

    feat(test): add edit report and download report tests

--- client/cypress/support/pages/GeneralReportPage.ts
+++ client/cypress/support/pages/GeneralReportPage.ts
@@ -31,10 +31,19 @@ export class GeneralReportPage {
   clickUpdateButton() {
     cy.get('[data-testid=report-submit-button]').click();
   }
+  clickSaveAsDraftButton() {
+    cy.get('[data-testid=report-save-as-draft-button]').click();
+  }
+  clickApplyChangesButton() {
+    cy.get('[data-testid=report-apply-changes-month-button]').click();
+  }
   clickConfirmationModalConfirmButton() {
     cy.get('[data-testid=confirmation-modal-confirm-button]').click();
   }
   clickConfirmationModalCancelButton() {
     cy.get('[data-testid=confirmation-modal-cancel-button]').click();
+  }
+  clickDownloadExcelButton() {
+    cy.get('[data-testid=report-download-excel-button]').click();
   }
 }
