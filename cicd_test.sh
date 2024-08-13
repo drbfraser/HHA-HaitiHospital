6495d1e95f0aa6e60e1cab51979a0c9a44146e0d
Ikenna Okpala <ikennajesse77@gmail.com>
2024-08-13 12:26:44 -0700

    chore(cicd): Test
    Issue: https://github.sfu.ca/bfraser/415-HHA-Haiti/issues/745

--- cicd_test.sh
+++ cicd_test.sh
@@ -1,15 +1,29 @@
 #!/bin/bash
 
+RED='\033[1;31m'
+BLUE='\033[1;36m'
+COLOR_OFF='\033[0m'
+
 set -x
 set -e
+
+echo -e "${BLUE}"
+echo -e "Running common tests"
+
+npm -v
+node -v
 
 cd common
 
 npm ci
 
+! npm run test && exit 1
+
 npm run build
 
 cd ..
+
+echo -e "Running server tests and build"
 
 cd server
 
@@ -17,11 +31,13 @@ npm ci
 
 npm run seed
 
-npm run test
+! npm run test && exit 1
 
 npm run start &
 
 cd ..
+
+echo -e "Running client"
 
 cd client
 
